f18f2a54620ff39841a1604aad6e3f4f1fbb2fe2
Michael Scoff <thetrustwallet@gmail.com>
2017-09-25 03:34:51 -0700

    Add taking screenshots steps

--- TrustUITests/Screenshots.swift
+++ TrustUITests/Screenshots.swift
@@ -11,7 +11,30 @@ class Screenshots: XCTestCase {
         app.launch()
     }
 
-    func testLaunch() {
+    func testMakeScreenshots() {
+
+        let app = XCUIApplication()
+
         snapshot("0Launch")
+
+        app.buttons["GET STARTED"].tap()
+
+        snapshot("1Create Wallet")
+
+        app.tables.staticTexts["Demo"].tap()
+
+        snapshot("3Transactions")
+
+        app.buttons["Send"].tap()
+
+        snapshot("2SendAndReceive")
+
+        app.navigationBars["Send ETH"].buttons["Cancel"].tap()
+
+        app.tables.buttons["Show my tokens"].tap()
+
+        sleep(12)
+
+        snapshot("4Tokens")
     }
 }
