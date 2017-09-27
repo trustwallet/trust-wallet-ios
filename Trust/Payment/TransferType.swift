81a580d50afa758bc4d84c4926fb3a953a15d7a2
Michael Scoff <thetrustwallet@gmail.com>
2017-09-27 06:04:20 -0700

    Add symbol to TransferType

--- Trust/Payment/TransferType.swift
+++ Trust/Payment/TransferType.swift
@@ -6,3 +6,14 @@ enum TransferType {
     case ether
     case token(Token)
 }
+
+extension TransferType {
+    var symbol: String {
+        switch self {
+        case .ether:
+            return "ETH"
+        case .token(let token):
+            return token.symbol
+        }
+    }
+}
