b0187b942ff5a5b739b37b3b9a30fde027751d84
Michael Scoff <thetrustwallet@gmail.com>
2017-09-28 16:16:54 -0700

    Update screenshots capturing for canceling send view

--- TrustUITests/Screenshots.swift
+++ TrustUITests/Screenshots.swift
@@ -25,7 +25,8 @@ class Screenshots: XCTestCase {
         app.buttons["Send"].tap()
         XCUIApplication().tables/*@START_MENU_TOKEN@*/.textFields["ETH Amount"]/*[[".cells.textFields[\"ETH Amount\"]",".textFields[\"ETH Amount\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         snapshot("2SendRequest")
-        app.navigationBars["Send ETH"].buttons["Cancel"].tap()
+
+        app.navigationBars["Trust.SendAndRequestViewContainer"].buttons["Cancel"].tap()
 
         snapshot("3Transactions")
 
