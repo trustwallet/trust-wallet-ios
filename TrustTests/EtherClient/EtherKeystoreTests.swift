ffc6b93bf3068e4b1d9c85b1830aa4e27a5862e6
Michael Scoff <thetrustwallet@gmail.com>
2017-09-28 21:07:46 -0700

    Add testImportFailInvalidPassword test case

--- TrustTests/EtherClient/EtherKeystoreTests.swift
+++ TrustTests/EtherClient/EtherKeystoreTests.swift
@@ -39,11 +39,10 @@ class EtherKeystoreTests: XCTestCase {
 
     func testImport() {
         let keystore = FakeEtherKeystore()
-        let password = "test"
 
         let result = keystore.importKeystore(
             value: "{\"address\":\"5e9c27156a612a2d516c74c7a80af107856f8539\",\"crypto\":{\"cipher\":\"aes-128-ctr\",\"ciphertext\":\"5eb0c790d1fb27824c78acac9233241b340c329b46aba08c6533b70ab67ea74f\",\"cipherparams\":{\"iv\":\"e5ab559977af075eda00a97c8f0ce506\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"n\":4096,\"p\":6,\"r\":8,\"salt\":\"b43142f34caf2b3b39c16f52344701f800711589f799cdae1827ac2f844f9602\"},\"mac\":\"c6ccaecca7896974dacac91a8116216ec287930bc74bfd7694a94f08bd992095\"},\"id\":\"e3554f73-4d0a-40a0-b721-fc801623d5ba\",\"version\":3}",
-            password: password
+            password: "test"
         )
 
         guard case let .success(account) = result else {
@@ -52,6 +51,21 @@ class EtherKeystoreTests: XCTestCase {
 
         XCTAssertEqual("0x5E9c27156a612a2D516C74c7a80af107856F8539", account.address.address)
         XCTAssertEqual(1, keystore.accounts.count)
+    }
+
+    func testImportFailInvalidPassword() {
+        let keystore = FakeEtherKeystore()
+
+        let result = keystore.importKeystore(
+            value: "{\"address\":\"5e9c27156a612a2d516c74c7a80af107856f8539\",\"crypto\":{\"cipher\":\"aes-128-ctr\",\"ciphertext\":\"5eb0c790d1fb27824c78acac9233241b340c329b46aba08c6533b70ab67ea74f\",\"cipherparams\":{\"iv\":\"e5ab559977af075eda00a97c8f0ce506\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"n\":4096,\"p\":6,\"r\":8,\"salt\":\"b43142f34caf2b3b39c16f52344701f800711589f799cdae1827ac2f844f9602\"},\"mac\":\"c6ccaecca7896974dacac91a8116216ec287930bc74bfd7694a94f08bd992095\"},\"id\":\"e3554f73-4d0a-40a0-b721-fc801623d5ba\",\"version\":3}",
+            password: "invalidPassword"
+        )
+
+        guard case .failure = result else {
+            return XCTFail()
+        }
+
+        XCTAssertEqual(0, keystore.accounts.count)
     }
 
     func testRecentlyUsedAccount() {
