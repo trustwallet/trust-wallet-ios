e3de23bef1f80ef254b0fd58061ec3b6591bd421
Michael Scoff <thetrustwallet@gmail.com>
2017-10-01 15:01:59 -0700

    Return error message from RPC server

--- Trust/Extensions/Error.swift
+++ Trust/Extensions/Error.swift
@@ -1,12 +1,30 @@
 // Copyright SIX DAY LLC. All rights reserved.
 
 import Foundation
+import APIKit
+import JSONRPCKit
 
 extension Error {
     var prettyError: String {
         switch self {
         case let error as LocalizedError:
             return error.errorDescription ?? "An unknown error occurred."
+        case let error as APIKit.SessionTaskError:
+            switch error {
+            case .connectionError(let error):
+                return error.localizedDescription
+            case .requestError(let error):
+                return error.localizedDescription
+            case .responseError(let error):
+                guard let JSONError = error as? JSONRPCError else {
+                    return error.localizedDescription
+                }
+                switch JSONError {
+                case .responseError(_, let message, _):
+                    return message
+                default: return "Undefined error"
+                }
+            }
         case let error as NSError:
             return error.localizedDescription
         default:
