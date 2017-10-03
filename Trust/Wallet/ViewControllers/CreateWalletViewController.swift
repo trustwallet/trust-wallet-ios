d60c27e1a0c0c9d3e360e2e5d57a24b6a026bfd1
Michael Scoff <thetrustwallet@gmail.com>
2017-10-03 00:56:54 -0700

    Change copy

--- Trust/Wallet/ViewControllers/CreateWalletViewController.swift
+++ Trust/Wallet/ViewControllers/CreateWalletViewController.swift
@@ -39,7 +39,7 @@ class CreateWalletViewController: FormViewController {
                 var header = HeaderFooterView<InfoHeaderView>(.class)
                 header.height = { 90 }
                 header.onSetupView = { (view, section) -> Void in
-                    view.label.attributedText = "Password will be used to encrypt your digital wallet".styled(
+                    view.label.attributedText = "Password will be used to encrypt and store your digital wallet".styled(
                         with:
                         .color(UIColor(hex: "6e6e72")),
                         .font(UIFont.systemFont(ofSize: 15, weight: UIFontWeightRegular)),
