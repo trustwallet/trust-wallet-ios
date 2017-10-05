9a4f06adc009857461e835f983c0914edc1d1984
Michael Scoff <thetrustwallet@gmail.com>
2017-10-04 23:46:29 -0700

    Handle AnyError properly

--- Trust/Extensions/Error.swift
+++ Trust/Extensions/Error.swift
@@ -3,28 +3,33 @@
 import Foundation
 import APIKit
 import JSONRPCKit
+import Result
 
 extension Error {
     var prettyError: String {
         switch self {
+        case let error as AnyError:
+            switch error.error {
+            case let error as APIKit.SessionTaskError:
+                switch error {
+                case .connectionError(let error):
+                    return error.localizedDescription
+                case .requestError(let error):
+                    return error.localizedDescription
+                case .responseError(let error):
+                    guard let JSONError = error as? JSONRPCError else {
+                        return error.localizedDescription
+                    }
+                    switch JSONError {
+                    case .responseError(_, let message, _):
+                        return message
+                    default: return "Undefined error"
+                    }
+                }
+            default: return error.description
+            }
         case let error as LocalizedError:
             return error.errorDescription ?? "An unknown error occurred."
-        case let error as APIKit.SessionTaskError:
-            switch error {
-            case .connectionError(let error):
-                return error.localizedDescription
-            case .requestError(let error):
-                return error.localizedDescription
-            case .responseError(let error):
-                guard let JSONError = error as? JSONRPCError else {
-                    return error.localizedDescription
-                }
-                switch JSONError {
-                case .responseError(_, let message, _):
-                    return message
-                default: return "Undefined error"
-                }
-            }
         case let error as NSError:
             return error.localizedDescription
         default:
@@ -35,3 +40,4 @@ extension Error {
     var code: Int { return (self as NSError).code }
     var domain: String { return (self as NSError).domain }
 }
+
