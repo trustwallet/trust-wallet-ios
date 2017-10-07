335db8901a98e6aa2100de61bffc078db0a796a6
Michael Scoff <thetrustwallet@gmail.com>
2017-10-07 03:41:19 -0700

    Add OnePasswordConverter

---
+++ Trust/Wallet/Types/OnePasswordConverter.swift
@@ -0,0 +1,20 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+struct OnePasswordConverter {
+
+    static let divider = "-trust-"
+
+    static func toPassword(password: String, keystore: String) -> String {
+        return password + divider + keystore
+    }
+
+    static func fromPassword(password: String) -> Result<(password: String, keystore: String), OnePasswordError> {
+        let split = password.components(separatedBy: divider)
+        if split.count >= 2 {
+            return .success((password: split[0], keystore: split[1]))
+        }
+        return .failure(OnePasswordError.wrongFormat)
+    }
+}
