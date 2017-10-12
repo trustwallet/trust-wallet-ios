e9fbffd05f6db6773dee63fa9fb2487c29386818
Michael Scoff <thetrustwallet@gmail.com>
2017-10-12 02:45:07 -0700

    Rename zip file

--- scripts/localizer.sh
+++ scripts/localizer.sh
@@ -3,4 +3,4 @@ find $1 -name "*.swift" ! -path "$1/R.generated.swift" -print0 | xargs -0 xcrun 
 
 tools/lokalise --token $2 import 3947163159df13df851b51.98101647 --file Localizable.strings --lang_iso en
 tools/lokalise --token $2 export 3947163159df13df851b51.98101647 --type strings --bundle_structure %LANG_ISO%.lproj/Localizable.%FORMAT%
-unzip -o Trust.zip -d $1/Localization
+unzip -o Trust-Localizable.zip -d $1/Localization
