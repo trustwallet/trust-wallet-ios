60d7069eac7754a450b4446edc5d1b4641a8a9f7
Michael Scoff <thetrustwallet@gmail.com>
2017-10-12 00:51:21 -0700

    Add Localized function

---
+++ Trust/Extensions/Functions.swift
@@ -0,0 +1,7 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+func Localized(_ key: String, value: String) -> String {
+    return NSLocalizedString(key, value: value, comment: "")
+}
