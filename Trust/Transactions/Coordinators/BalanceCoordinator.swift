5322de597a910db18d867d79cbd8eb90246ec228
Michael Scoff <thetrustwallet@gmail.com>
2017-10-11 19:22:52 -0700

    Add BalanceCoordinator

---
+++ Trust/Transactions/Coordinators/BalanceCoordinator.swift
@@ -0,0 +1,71 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import APIKit
+import JSONRPCKit
+
+protocol BalanceCoordinatorDelegate: class {
+    func didUpdate(viewModel: BalanceViewModel)
+}
+
+class BalanceCoordinator {
+
+    let exchangeRateCoordinator = ExchangeRateCoordinator()
+    let account: Account
+
+    weak var delegate: BalanceCoordinatorDelegate?
+
+    var balance: Balance? {
+        didSet { update() }
+    }
+    var currencyRate: CurrencyRate? {
+        didSet { update() }
+    }
+
+    init(account: Account) {
+        self.account = account
+    }
+
+    func start() {
+        exchangeRateCoordinator.delegate = self
+        exchangeRateCoordinator.start()
+
+        fetchBalance()
+    }
+
+    func fetch() {
+        exchangeRateCoordinator.fetch()
+        fetchBalance()
+    }
+
+    func fetchBalance() {
+        let request = EtherServiceRequest(batch: BatchFactory().create(BalanceRequest(address: account.address.address)))
+        Session.send(request) { [weak self] result in
+            switch result {
+            case .success(let balance):
+                self?.balance = balance
+            case .failure:
+                break
+            }
+        }
+    }
+
+    func update() {
+        guard let balance = balance else {
+            return
+        }
+
+        let viewModel = BalanceViewModel(
+            balance: balance,
+            rate: currencyRate
+        )
+
+        delegate?.didUpdate(viewModel: viewModel)
+    }
+}
+
+extension BalanceCoordinator: ExchangeRateCoordinatorDelegate {
+    func didUpdate(rate: CurrencyRate, in coordinator: ExchangeRateCoordinator) {
+        currencyRate = rate
+    }
+}
