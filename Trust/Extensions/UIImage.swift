2e4fb19283c9d97e04a4c875a51f0f0c5f61062d
Michael Scoff <thetrustwallet@gmail.com>
2017-10-13 15:56:52 -0700

    Add UIImage filled with color extension

---
+++ Trust/Extensions/UIImage.swift
@@ -0,0 +1,20 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static func filled(with color: UIColor) -> UIImage {
+        let pixelScale = UIScreen.main.scale
+        let pixelSize = 1 / pixelScale
+        let fillSize = CGSize(width: pixelSize, height: pixelSize)
+        let fillRect = CGRect(origin: CGPoint.zero, size: fillSize)
+        UIGraphicsBeginImageContextWithOptions(fillRect.size, false, pixelScale)
+        let graphicsContext = UIGraphicsGetCurrentContext()
+        graphicsContext!.setFillColor(color.cgColor)
+        graphicsContext!.fill(fillRect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
