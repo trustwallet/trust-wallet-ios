ce9aa195a96571fa93a5a727a8f8b33a1d113cae
Michael Scoff <thetrustwallet@gmail.com>
2017-10-13 15:57:38 -0700

    Add image view and label to AccountViewCell

--- Trust/Accounts/Views/AccountViewCell.swift
+++ Trust/Accounts/Views/AccountViewCell.swift
@@ -6,8 +6,37 @@ class AccountViewCell: UITableViewCell {
 
     static let identifier = "AccountViewCell"
 
+    let walletImageView = UIImageView()
+    let walletLabel = UILabel()
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        walletImageView.translatesAutoresizingMaskIntoConstraints = false
+        walletImageView.layer.cornerRadius = 6
+        walletImageView.layer.masksToBounds = true
+
+        walletLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addSubview(walletImageView)
+        contentView.addSubview(walletLabel)
+
+        NSLayoutConstraint.activate([
+            walletImageView.widthAnchor.constraint(equalToConstant: 34),
+            walletImageView.heightAnchor.constraint(equalToConstant: 34),
+            walletImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            walletImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Layout.sideMargin),
+
+            walletLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.sideMargin),
+            walletLabel.leadingAnchor.constraint(equalTo: walletImageView.trailingAnchor, constant: Layout.sideMargin),
+            walletLabel.topAnchor.constraint(equalTo: topAnchor),
+            walletLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+
+    func configure(viewModel: AccountViewModel) {
+        walletImageView.image = viewModel.image
+        walletLabel.text = viewModel.title
     }
 
     required init?(coder aDecoder: NSCoder) {
