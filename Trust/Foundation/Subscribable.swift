2979a4ff5710308c68782315dd09752fed9a6357
Michael Scoff <thetrustwallet@gmail.com>
2017-10-15 13:45:29 -0700

    Add Subscribable

---
+++ Trust/Foundation/Subscribable.swift
@@ -0,0 +1,30 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+open class Subscribable<T> {
+    private var _value: T?
+    private var _subscribers: [(T?) -> Void] = []
+    open var value: T? {
+        get {
+            return _value
+        }
+        set {
+            _value = newValue
+            for f in _subscribers {
+                f(value)
+            }
+        }
+    }
+
+    public init(_ value: T?) {
+        _value = value
+    }
+
+    open func subscribe(_ subscribe: @escaping (T?) -> Void) {
+        if let value = _value {
+            subscribe(value)
+        }
+        _subscribers.append(subscribe)
+    }
+}
