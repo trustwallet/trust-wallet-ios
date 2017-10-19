8b729aeb9c82d538a0bf41a1e9fb4afc446ee7d1
Michael Scoff <thetrustwallet@gmail.com>
2017-10-18 17:06:26 -0700

    Add UnconfirmedTransaction

---
+++ Trust/Transfer/Types/UnconfirmedTransaction.swift
@@ -0,0 +1,9 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+struct UnconfirmedTransaction {
+    let transferType: TransferType
+    let amount: Double
+    let address: Address
+}
