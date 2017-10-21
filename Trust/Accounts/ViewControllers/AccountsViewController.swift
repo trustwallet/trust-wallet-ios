f27f7352155f0e1cb560d92e40aa3d820145f9ed
Michael Scoff <thetrustwallet@gmail.com>
2017-10-21 16:53:54 -0700

    Revert "Call delegate before fetch"
    This reverts commit eac75342c8f8b9e7234dcba9d7bb380d0752c398.

--- Trust/Accounts/ViewControllers/AccountsViewController.swift
+++ Trust/Accounts/ViewControllers/AccountsViewController.swift
@@ -107,8 +107,8 @@ class AccountsViewController: UITableViewController {
         let result = self.keystore.delete(account: account)
         switch result {
         case .success:
-            self.delegate?.didDeleteAccount(account: account, in: self)
             self.fetch()
+            self.delegate?.didDeleteAccount(account: account, in: self)
         case .failure(let error):
             self.displayError(error: error)
         }
