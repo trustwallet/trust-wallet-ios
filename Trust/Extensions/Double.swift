4a19209450393487dcdc9edd2d7912576bfd9bf9
Michael Scoff <thetrustwallet@gmail.com>
2017-10-24 00:55:51 -0700

    Add rounded extension

---
+++ Trust/Extensions/Double.swift
@@ -0,0 +1,10 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+extension Double {
+    func rounded(to places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
