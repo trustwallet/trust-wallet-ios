a80cc7a3700b2b26e74444020db1c6a33f5b9734
Michael Scoff <thetrustwallet@gmail.com>
2017-10-23 17:15:00 -0700

    Split login into BackUpCoordinator

--- Trust/Export/Coordinators/ExportCoordinator.swift
+++ Trust/Export/Coordinators/ExportCoordinator.swift
@@ -43,70 +43,34 @@ class ExportCoordinator: Coordinator {
         delegate?.didFinish(in: self)
     }
 
-    func presentActivityViewController(for account: Account, password: String, newPassword: String, completion: @escaping () -> Void) {
-        let result = keystore.export(account: account, password: password, newPassword: newPassword)
-
-        switch result {
-        case .success(let value):
-            let activityViewController = UIActivityViewController(
-                activityItems: [value],
-                applicationActivities: nil
-            )
-            activityViewController.completionWithItemsHandler = { result in
-                completion()
-            }
-            activityViewController.popoverPresentationController?.sourceView = self.accountViewController.view
-            rootNavigationController.present(activityViewController, animated: true, completion: nil)
-        case .failure(let error):
-            rootNavigationController.displayError(error: error)
-        }
-    }
-
-    func presentShareActivity(for account: Account, password: String, newPassword: String) {
-        self.presentActivityViewController(for: account, password: password, newPassword: newPassword) {
-            self.finish()
-        }
+    func export(for account: Account) {
+        let coordinator = BackupCoordinator(
+            navigationController: rootNavigationController,
+            account: account
+        )
+        addCoordinator(coordinator)
+        coordinator.start()
     }
 }
 
 extension ExportCoordinator: AccountsViewControllerDelegate {
     func didSelectAccount(account: Account, in viewController: AccountsViewController) {
-
-        if let currentPassword = keystore.getPassword(for: account) {
-            let verifyController = UIAlertController.askPassword(
-                title: NSLocalizedString("export.enterNewPasswordWallet", value: "Enter new password to export your wallet", comment: "")
-            ) { result in
-                switch result {
-                case .success(let newPassword):
-                    self.presentShareActivity(
-                        for: account,
-                        password: currentPassword,
-                        newPassword: newPassword
-                    )
-                case .failure: break
-                }
-            }
-            rootNavigationController.present(verifyController, animated: true, completion: nil)
-        } else {
-            //FIXME: remove later. for old version, when password were missing in the keychain
-            let verifyController = UIAlertController.askPassword(
-                title: NSLocalizedString("export.enterCurrentPasswordWallet", value: "Enter current password to export your wallet", comment: "")
-            ) { result in
-                switch result {
-                case .success(let newPassword):
-                    self.presentShareActivity(
-                        for: account,
-                        password: newPassword,
-                        newPassword: newPassword
-                    )
-                case .failure: break
-                }
-            }
-            rootNavigationController.present(verifyController, animated: true, completion: nil)
-        }
+        export(for: account)
     }
 
     func didDeleteAccount(account: Account, in viewController: AccountsViewController) {
 
+    }
+}
+
+extension ExportCoordinator: BackupCoordinatorDelegate {
+    func didFinish(account: Account, in coordinator: BackupCoordinator) {
+        removeCoordinator(coordinator)
+        delegate?.didFinish(in: self)
+    }
+
+    func didCancel(coordinator: BackupCoordinator) {
+        removeCoordinator(coordinator)
+        delegate?.didCancel(in: self)
     }
 }
