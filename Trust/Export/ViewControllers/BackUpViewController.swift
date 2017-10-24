2e2db1342041f5c59eacff73ba15d187f2f757d1
Michael Scoff <thetrustwallet@gmail.com>
2017-10-23 17:14:04 -0700

    Add BackupViewController

---
+++ Trust/Export/ViewControllers/BackUpViewController.swift
@@ -0,0 +1,101 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import UIKit
+
+protocol BackupViewControllerDelegate: class {
+    func didPressBackup(account: Account, in viewController: BackupViewController)
+    func didPressLater(account: Account, in viewController: BackupViewController)
+}
+
+class BackupViewController: UIViewController {
+
+    let account: Account
+    weak var delegate: BackupViewControllerDelegate?
+
+    init(account: Account) {
+        self.account = account
+
+        super.init(nibName: nil, bundle: nil)
+
+        let warningImageView = UIImageView()
+        warningImageView.translatesAutoresizingMaskIntoConstraints = false
+        warningImageView.image = R.image.export_warning()
+
+        let noBackupLabel = UILabel()
+        noBackupLabel.translatesAutoresizingMaskIntoConstraints = false
+        noBackupLabel.text = NSLocalizedString("export.noBackup.title", value: "No backup, no Ethereum.", comment: "")
+        noBackupLabel.font = UIFont.systemFont(ofSize: 22, weight: UIFontWeightMedium)
+
+        let controlMoneyLabel = UILabel()
+        controlMoneyLabel.translatesAutoresizingMaskIntoConstraints = false
+        controlMoneyLabel.text = NSLocalizedString("export.controlYourMoney.title", value: "Since only you control your money, you'll need to save your backup keystore in case this app is deleted.", comment: "")
+        controlMoneyLabel.numberOfLines = 0
+        controlMoneyLabel.textAlignment = .center
+        controlMoneyLabel.textColor = Colors.darkGray
+
+        let neverStoredLabel = UILabel()
+        neverStoredLabel.translatesAutoresizingMaskIntoConstraints = false
+        neverStoredLabel.text = NSLocalizedString("export.neverStored.title", value: "Your wallet is never saved to cloud storage or standart device backups.", comment: "")
+        neverStoredLabel.numberOfLines = 0
+        neverStoredLabel.textAlignment = .center
+        neverStoredLabel.textColor = Colors.darkGray
+
+        let backupButton = Button(size: .large, style: .solid)
+        backupButton.translatesAutoresizingMaskIntoConstraints = false
+        backupButton.setTitle(NSLocalizedString("export.backupButton.title", value: "Backup Wallet", comment: ""), for: .normal)
+        backupButton.addTarget(self, action: #selector(backup), for: .touchUpInside)
+
+        let doItLaterButton = Button(size: .large, style: .border)
+        doItLaterButton.translatesAutoresizingMaskIntoConstraints = false
+        doItLaterButton.setTitle(NSLocalizedString("export.doItLater.title", value: "Do it later", comment: ""), for: .normal)
+        doItLaterButton.addTarget(self, action: #selector(later), for: .touchUpInside)
+
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                warningImageView,
+                .spacer(),
+                noBackupLabel,
+                .spacer(height: 20),
+                controlMoneyLabel,
+                neverStoredLabel,
+                .spacer(height: 20),
+                backupButton,
+                doItLaterButton,
+            ]
+        )
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.backgroundColor = .white
+        navigationItem.title = "Backup"
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(greaterThanOrEqualTo: view.layoutGuide.topAnchor, constant: StyleLayout.sideMargin),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.layoutGuide.leadingAnchor, constant: StyleLayout.sideMargin),
+            stackView.trailingAnchor.constraint(equalTo: view.layoutGuide.trailingAnchor, constant: -StyleLayout.sideMargin),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: view.layoutGuide.bottomAnchor, constant: -StyleLayout.sideMargin),
+
+            backupButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            backupButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            doItLaterButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            doItLaterButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+        ])
+    }
+
+    func backup() {
+        delegate?.didPressBackup(account: account, in: self)
+    }
+
+    func later() {
+        delegate?.didPressLater(account: account, in: self)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
