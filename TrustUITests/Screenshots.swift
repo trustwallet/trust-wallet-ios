7bc05ed7812f96909abbb623d0c33d07c5e79929
Michael Scoff <thetrustwallet@gmail.com>
2017-10-26 12:31:23 -0700

    Updated screenshots

--- TrustUITests/Screenshots.swift
+++ TrustUITests/Screenshots.swift
@@ -18,20 +18,21 @@ class Screenshots: XCTestCase {
 
         app.buttons["IMPORT WALLET"].tap()
 
-        snapshot("4ImportWallet")
+        snapshot("3ImportWallet")
         sleep(4)
         app.navigationBars["Import Wallet"].buttons["Demo"].tap()
 
         app.buttons["Send"].tap()
         app.tables.textFields["ETH Amount"].tap()
-        snapshot("2SendRequest")
+
+        snapshot("1SendRequest")
 
         app.navigationBars["Send ETH"].buttons["Cancel"].tap()
 
-        snapshot("3Transactions")
+        snapshot("4Transactions")
         
         app.tables.buttons["Show my tokens"].tap()
         sleep(12)
-        snapshot("1Tokens")
+        snapshot("2Tokens")
     }
 }
