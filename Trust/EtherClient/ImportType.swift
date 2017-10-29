c44da6573b24a3da399bdea4797e1969f6508725
Michael Scoff <thetrustwallet@gmail.com>
2017-10-29 13:41:23 -0700

    Remove Russian localization
    Add ImportType

---
+++ Trust/EtherClient/ImportType.swift
@@ -0,0 +1,8 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+enum ImportType {
+    case keystore(string: String, password: String)
+    case privateKey(privateKey: String, password: String)
+}
