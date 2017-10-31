e0595302d7c8f6b4ca713c6e77ee397630ec7202
Michael Scoff <thetrustwallet@gmail.com>
2017-10-30 19:34:28 -0700

    Remove warnings

--- Trust/Extensions/String.swift
+++ Trust/Extensions/String.swift
@@ -57,9 +57,9 @@ extension String {
 
 extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
     var jsonString: String? {
-        if let dict = (self as AnyObject) as? Dictionary<String, AnyObject> {
+        if let dict = (self as AnyObject) as? [String: AnyObject] {
             do {
-                let data = try JSONSerialization.data(withJSONObject: dict, options: JSONSerialization.WritingOptions(rawValue: UInt.allZeros))
+                let data = try JSONSerialization.data(withJSONObject: dict, options: JSONSerialization.WritingOptions(rawValue: 0))
                 if let string = String(data: data, encoding: String.Encoding.utf8) {
                     return string
                 }
