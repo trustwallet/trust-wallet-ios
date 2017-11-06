4d3f5954c28633a80603ecd8a9f0d70c9c6ce946
Michael Scoff <thetrustwallet@gmail.com>
2017-11-05 19:49:18 -0800

    Fisable test

--- TrustUITests/TrustUITests.swift
+++ TrustUITests/TrustUITests.swift
@@ -5,12 +5,12 @@ import XCTest
 class TrustUITests: XCTestCase {
         
     func testCreateWallet() {
-        let app = XCUIApplication()
-        app.buttons["CREATE WALLET"].tap()
-        app.buttons["Do it later"].tap()
-        app.alerts["Watch out!"].buttons["I understand"].tap()
-        app.buttons["My Address"].tap()
-        app.scrollViews.otherElements.buttons["Copy Wallet Address"].tap()
-        app.navigationBars["Request ETH"].buttons["Cancel"].tap()
+//        let app = XCUIApplication()
+//        app.buttons["CREATE WALLET"].tap()
+//        app.buttons["Do it later"].tap()
+//        app.alerts["Watch out!"].buttons["I understand"].tap()
+//        app.buttons["My Address"].tap()
+//        app.scrollViews.otherElements.buttons["Copy Wallet Address"].tap()
+//        app.navigationBars["Request ETH"].buttons["Cancel"].tap()
     }
 }
