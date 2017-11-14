97f9a7f4fcf67d05f63d82e0075df207476b7fea
Michael Scoff <thetrustwallet@gmail.com>
2017-11-13 16:45:49 -0800

    Add ContainerView

---
+++ Trust/UI/ContainerView.swift
@@ -0,0 +1,38 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import UIKit
+
+class ContainerView: UIView {
+
+    public init(innerView: UIView, layoutMargins: UIEdgeInsets) {
+
+        super.init(frame: .zero)
+
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.preservesSuperviewLayoutMargins = true
+        containerView.layoutMargins = layoutMargins
+        containerView.addSubview(innerView)
+        addSubview(containerView)
+
+        innerView.translatesAutoresizingMaskIntoConstraints = false
+        innerView.preservesSuperviewLayoutMargins = true
+
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: topAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+
+            innerView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            innerView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            innerView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            innerView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+        ])
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
