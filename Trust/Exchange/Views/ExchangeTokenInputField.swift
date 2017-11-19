2dec26ed5220e12a6577ceec7da4d7ee938daa53
Michael Scoff <thetrustwallet@gmail.com>
2017-11-19 03:36:19 -0800

    Organizing exchange field views

--- Trust/Exchange/Views/ExchangeTokenInputField.swift
+++ Trust/Exchange/Views/ExchangeTokenInputField.swift
@@ -4,19 +4,95 @@ import UIKit
 
 class ExchangeTokenInputField: UIView {
 
-    let valueInput  = ExchangeValueInput()
-    let tokenView = ExchangeTokenView()
+    let floatLabelView = UIView()
+    let destinationLabel = UILabel()
+    let amountField = UITextField()
+
+    let tokenView = UIView()
+    let tokenImageView = UIImageView()
+    let label = UILabel()
 
     init() {
 
         super.init(frame: .zero)
 
-        valueInput.translatesAutoresizingMaskIntoConstraints = false
+        // value
+
+        floatLabelView.translatesAutoresizingMaskIntoConstraints = false
+
+        destinationLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        amountField.translatesAutoresizingMaskIntoConstraints = false
+        amountField.backgroundColor = .red
+        amountField.font = UIFont.systemFont(ofSize: 26, weight: UIFontWeightMedium)
+
+        let inputStackView = UIStackView(arrangedSubviews: [
+            destinationLabel,
+            amountField,
+        ])
+        inputStackView.translatesAutoresizingMaskIntoConstraints = false
+        inputStackView.axis = .vertical
+        inputStackView.spacing = 5
+
+        addSubview(floatLabelView)
+        floatLabelView.addSubview(inputStackView)
+
+        NSLayoutConstraint.activate([
+            inputStackView.centerYAnchor.constraint(equalTo: floatLabelView.centerYAnchor),
+            inputStackView.leadingAnchor.constraint(equalTo: floatLabelView.leadingAnchor),
+            inputStackView.trailingAnchor.constraint(equalTo: floatLabelView.trailingAnchor, constant: -15),
+
+            floatLabelView.topAnchor.constraint(equalTo: topAnchor),
+            floatLabelView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            floatLabelView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+
+        // token view
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "ETH"
+
+        tokenImageView.translatesAutoresizingMaskIntoConstraints = false
+        tokenImageView.image = R.image.accounts_active()
 
         tokenView.translatesAutoresizingMaskIntoConstraints = false
+        tokenView.backgroundColor = .green
+
+        let tokenStackView = UIStackView(arrangedSubviews: [
+            tokenImageView,
+            label,
+        ])
+        tokenStackView.translatesAutoresizingMaskIntoConstraints = false
+        tokenStackView.axis = .vertical
+        tokenStackView.spacing = 5
+
+        tokenView.addSubview(tokenStackView)
+        addSubview(tokenView)
+
+        let chevronDownImageView = UIImageView(image: R.image.chevronDown())
+        chevronDownImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        tokenView.addSubview(chevronDownImageView)
+
+        NSLayoutConstraint.activate([
+            tokenStackView.centerXAnchor.constraint(equalTo: tokenView.centerXAnchor),
+            tokenStackView.centerYAnchor.constraint(equalTo: tokenView.centerYAnchor),
+
+            tokenView.topAnchor.constraint(equalTo: topAnchor),
+            tokenView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tokenView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tokenView.widthAnchor.constraint(equalToConstant: 100),
+            tokenView.leadingAnchor.constraint(equalTo: floatLabelView.trailingAnchor),
+
+            chevronDownImageView.leadingAnchor.constraint(equalTo: tokenStackView.trailingAnchor),
+            chevronDownImageView.centerYAnchor.constraint(equalTo: tokenStackView.centerYAnchor),
+        ])
+
+        // main stack
 
         let stackView = UIStackView(arrangedSubviews: [
-            valueInput,
+            floatLabelView,
             tokenView,
         ])
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,7 +105,7 @@ class ExchangeTokenInputField: UIView {
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            stackView.heightAnchor.constraint(equalToConstant: 120),
+            stackView.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
 
--- Trust/Exchange/Views/ExchangeTokensField.swift
+++ Trust/Exchange/Views/ExchangeTokensField.swift
@@ -6,13 +6,23 @@ class ExchangeTokensField: UIView {
 
     let to = ExchangeTokenInputField()
     let from = ExchangeTokenInputField()
+    let swapButton = UIButton()
 
     init() {
 
         super.init(frame: .zero)
 
         to.translatesAutoresizingMaskIntoConstraints = false
+        to.destinationLabel.text = "You send"
+
         from.translatesAutoresizingMaskIntoConstraints = false
+        from.destinationLabel.text = "You get"
+
+        swapButton.translatesAutoresizingMaskIntoConstraints = false
+        swapButton.setImage(R.image.swap(), for: .normal)
+        swapButton.addTarget(self, action: #selector(swap), for: .touchUpInside)
+        swapButton.layer.cornerRadius = 20
+        swapButton.backgroundColor = .white
 
         let stackView = UIStackView(arrangedSubviews: [
             to,
@@ -22,13 +32,23 @@ class ExchangeTokensField: UIView {
         stackView.axis = .vertical
 
         addSubview(stackView)
+        addSubview(swapButton)
 
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: topAnchor),
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            swapButton.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+            swapButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -80),
+            swapButton.widthAnchor.constraint(equalToConstant: 40),
+            swapButton.heightAnchor.constraint(equalToConstant: 40),
         ])
+    }
+
+    func swap() {
+
     }
 
     required init?(coder aDecoder: NSCoder) {
