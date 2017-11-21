41db23f3fec5c234cb874fca5283ec50bcc4ddd7
Michael Scoff <thetrustwallet@gmail.com>
2017-11-21 10:58:09 -0800

    Add InCoordinatorViewModel

---
+++ Trust/Core/ViewModels/InCoordinatorViewModel.swift
@@ -0,0 +1,23 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+struct InCoordinatorViewModel {
+
+    let config: Config
+
+    init(config: Config) {
+        self.config = config
+    }
+
+    var tokensAvailable: Bool {
+        switch config.server {
+        case .main: return true
+        case .kovan, .oraclesTest: return false
+        }
+    }
+
+    var exchangeAvailable: Bool {
+        return false
+    }
+}
