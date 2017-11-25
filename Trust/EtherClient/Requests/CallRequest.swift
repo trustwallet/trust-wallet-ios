2289820e201e0c286d2730435955f2d869b28369
Michael Scoff <thetrustwallet@gmail.com>
2017-11-25 05:00:31 -0800

    Add CallRequest

---
+++ Trust/EtherClient/Requests/CallRequest.swift
@@ -0,0 +1,27 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import JSONRPCKit
+
+struct CallRequest: JSONRPCKit.Request {
+    typealias Response = String
+
+    let to: String
+    let data: String
+
+    var method: String {
+        return "eth_call"
+    }
+
+    var parameters: Any? {
+        return [["to": to, "data": data]]
+    }
+
+    func response(from resultObject: Any) throws -> Response {
+        if let response = resultObject as? Response {
+            return response
+        } else {
+            throw CastError(actualValue: resultObject, expectedType: Response.self)
+        }
+    }
+}
