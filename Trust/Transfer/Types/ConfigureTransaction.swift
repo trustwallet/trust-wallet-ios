e9d99f9c1ae9dc66168de2fda06c02d88a448e26
Michael Scoff <thetrustwallet@gmail.com>
2017-12-02 04:01:01 -0800

    Add ConfigureTransaction and ConfigureTransactionError

---
+++ Trust/Transfer/Types/ConfigureTransaction.swift
@@ -0,0 +1,8 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+struct ConfigureTransaction {
+    static var gasLimitMax: Int = 6370515
+    static var gasFeeMax: Int64 = 1_000_000_000_000_000_000 / 10
+}
---
+++ Trust/Transfer/Types/ConfigureTransactionError.swift
@@ -0,0 +1,32 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import BigInt
+
+enum ConfigureTransactionError: LocalizedError {
+    case gasLimitTooHigh
+    case gasFeeTooHigh
+
+    var errorDescription: String? {
+        switch self {
+        case .gasLimitTooHigh:
+            return String(
+                format: NSLocalizedString(
+                    "configureTransaction.gasLimitTooHigh",
+                    value: "Gas Limit too high. Max available: %d",
+                    comment: ""
+                ),
+                ConfigureTransaction.gasLimitMax
+            )
+        case .gasFeeTooHigh:
+            return String(
+                format: NSLocalizedString(
+                    "configureTransaction.gasFeeTooHigh",
+                    value: "Gas Fee too high. Max available: %@",
+                    comment: ""
+                ),
+                String(ConfigureTransaction.gasFeeMax)
+            )
+        }
+    }
+}
