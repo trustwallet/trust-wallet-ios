badd3579d704dc8f86a0de56b4007a99dec50982
Michael Scoff <thetrustwallet@gmail.com>
2017-12-06 20:01:15 -0800

    Add GasPriceRequest

---
+++ Trust/EtherClient/Requests/GasPriceRequest.swift
@@ -0,0 +1,20 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import JSONRPCKit
+
+struct GasPriceRequest: JSONRPCKit.Request {
+    typealias Response = String
+
+    var method: String {
+        return "eth_gasPrice"
+    }
+
+    func response(from resultObject: Any) throws -> Response {
+        if let response = resultObject as? Response {
+            return response
+        } else {
+            throw CastError(actualValue: resultObject, expectedType: Response.self)
+        }
+    }
+}
