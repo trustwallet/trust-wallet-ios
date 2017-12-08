e087cdc3d817419b3ba9d1bc2be3f1641fc482ae
Michael Scoff <thetrustwallet@gmail.com>
2017-12-07 21:33:50 -0800

    Add extra random bytes to generateRandom()

--- Trust/EtherClient/PasswordGenerator.swift
+++ Trust/EtherClient/PasswordGenerator.swift
@@ -1,10 +1,17 @@
 // Copyright SIX DAY LLC. All rights reserved.
 
 import Foundation
+import Security
 
 struct PasswordGenerator {
 
     static func generateRandom() -> String {
-        return NSUUID().uuidString
+        return NSUUID().uuidString + PasswordGenerator.generateRandomString(bytesCount: 16)
+    }
+
+    static func generateRandomString(bytesCount: Int) -> String {
+        var randomBytes = [UInt8](repeating: 0, count: bytesCount)
+        let _ = SecRandomCopyBytes(kSecRandomDefault, bytesCount, &randomBytes)
+        return randomBytes.map({ String(format: "%02hhx", $0) }).joined(separator: "")
     }
 }
