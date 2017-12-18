a200879d8ace1906a5751182927c00668d8deb70
Michael Scoff <thetrustwallet@gmail.com>
2017-12-17 22:25:21 -0800

    Fix build compilation

--- Trust/EtherClient/Ethplorer/GetTokensRequest.swift
+++ Trust/EtherClient/Ethplorer/GetTokensRequest.swift
@@ -31,7 +31,7 @@ struct GetTokensRequest: APIKit.Request {
         if
             let objectJSON = object as? [String: AnyObject],
             let tokensJSON = objectJSON["tokens"] as? [[String: AnyObject]] {
-            return tokensJSON.map { Token.from(address: address, json: $0) }.filter { !$0.address.isEmpty }
+            return tokensJSON.map { Token.from(address: address, json: $0) }.filter { !$0.address.address.isEmpty }
         }
         return []
     }
