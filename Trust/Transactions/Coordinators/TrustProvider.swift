a804f5e28e9ee3b71b8420b2cbc1ac5e18805dd3
Miachel Scoff <thetrustwallet@gmail.com>
2017-12-23 12:34:12 -0800

    Disable pinning, until finished

--- Trust/Transactions/Coordinators/TrustProvider.swift
+++ Trust/Transactions/Coordinators/TrustProvider.swift
@@ -6,11 +6,13 @@ import Moya
 
 struct TrustProviderFactory {
     static let policies: [String: ServerTrustPolicy] = [
-        "trustwalletapp.com": .pinPublicKeys(
-            publicKeys: ServerTrustPolicy.publicKeys(in: Bundle.main),
-            validateCertificateChain: true,
-            validateHost: true
-        ),
+        :
+//        Disabled until: https://github.com/TrustWallet/trust-wallet-ios/pull/129#issuecomment-353718512
+//        "trustwalletapp.com": .pinPublicKeys(
+//            publicKeys: ServerTrustPolicy.publicKeys(in: Bundle.main),
+//            validateCertificateChain: true,
+//            validateHost: true
+//        ),
     ]
 
     static func makeProvider() -> MoyaProvider<TrustService> {
