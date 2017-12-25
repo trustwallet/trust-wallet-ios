d613ccc5e1c9dd760555e7eb32554e9eecdf2289
Michael Scoff <thetrustwallet@gmail.com>
2017-12-25 02:58:43 -0800

    Change copy for tokens footer

--- Trust/Tokens/ViewModels/TokensViewModel.swift
+++ Trust/Tokens/ViewModels/TokensViewModel.swift
@@ -81,7 +81,7 @@ struct TokensViewModel {
     }
 
     var footerTitle: String {
-        return NSLocalizedString("tokens.footer.label.title", value: "Missing token?", comment: "")
+        return NSLocalizedString("tokens.footer.label.title", value: "Tokens will appear automagically. + to add manually.", comment: "")
     }
 
     var footerTextColor: UIColor {
