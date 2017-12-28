8b0ed3b993f6a88af169422274d074c1ad12b326
Michael Scoff <thetrustwallet@gmail.com>
2017-12-28 02:19:38 -0800

    Select tokens view controller by default

--- Trust/InCoordinator.swift
+++ Trust/InCoordinator.swift
@@ -95,6 +95,10 @@ class InCoordinator: Coordinator {
             tokenCoordinator.start()
             addCoordinator(tokenCoordinator)
             tabBarController.viewControllers?.append(tokenCoordinator.navigationController)
+
+            if !transactionsStorage.objects.isEmpty {
+                tabBarController.selectedViewController = tokenCoordinator.navigationController
+            }
         }
 
         if inCoordinatorViewModel.exchangeAvailable {
