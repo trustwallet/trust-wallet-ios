6a8549a5b483eaae3c18b30342e9af16bba0435f
Michael Scoff <thetrustwallet@gmail.com>
2018-01-03 02:13:15 -0800

    Fix retain cycle on update balance

--- Trust/Settings/Coordinators/ExchangeRateCoordinator.swift
+++ Trust/Settings/Coordinators/ExchangeRateCoordinator.swift
@@ -26,7 +26,8 @@ class ExchangeRateCoordinator: NSObject {
     }
 
     func fetch() {
-        provider.request(.prices(currency: Config().currency, symbols:[config.server.symbol])) { result in
+        provider.request(.prices(currency: Config().currency, symbols:[config.server.symbol])) { [weak self] result in
+            guard let `self` = self else { return }
             guard case .success(let response) = result else { return }
             do {
                 guard let ticker = try response.map([CoinTicker].self, atKeyPath: "response", using: JSONDecoder()).first else { return }
