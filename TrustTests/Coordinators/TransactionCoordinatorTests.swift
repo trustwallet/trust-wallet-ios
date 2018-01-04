e3fb5ea48ef10b535ec6f451dd289a07e4675ae7
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-01-05 00:04:54 +0200

    Fix for Unit tests.

--- TrustTests/Coordinators/TransactionCoordinatorTests.swift
+++ TrustTests/Coordinators/TransactionCoordinatorTests.swift
@@ -5,4 +5,37 @@ import XCTest
 
 class TransactionCoordinatorTests: XCTestCase {
 
+    func testShowSendFlow() {
+        let coordinator = TransactionCoordinator(
+            session: .make(),
+            navigationController: FakeNavigationController(),
+            storage: FakeTransactionsStorage(),
+            keystore: FakeEtherKeystore(),
+            tokensStorage: FakeTokensDataStore()
+        )
+
+        coordinator.showPaymentFlow(for: .send(type: .ether(destination: .none)))
+
+        let controller = (coordinator.navigationController.presentedViewController as? UINavigationController)?.viewControllers[0]
+
+        XCTAssertTrue(coordinator.coordinators.first is PaymentCoordinator)
+        XCTAssertTrue(controller is SendViewController)
+    }
+
+    func testShowRequstFlow() {
+        let coordinator = TransactionCoordinator(
+            session: .make(),
+            navigationController: FakeNavigationController(),
+            storage: FakeTransactionsStorage(),
+            keystore: FakeEtherKeystore(),
+            tokensStorage: FakeTokensDataStore()
+        )
+
+        coordinator.showPaymentFlow(for: .request)
+
+        let controller = (coordinator.navigationController.presentedViewController as? UINavigationController)?.viewControllers[0]
+
+        XCTAssertTrue(coordinator.coordinators.first is PaymentCoordinator)
+        XCTAssertTrue(controller is RequestViewController)
+    }
 }
