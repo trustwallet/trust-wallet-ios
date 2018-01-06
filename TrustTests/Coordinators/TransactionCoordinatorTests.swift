2fff2225dd8aed7c3ffbbcb4faeb8f182f619882
Michael Scoff <thetrustwallet@gmail.com>
2018-01-06 04:02:22 -0800

    Moved tests into InCoordinatorTests

--- TrustTests/Coordinators/TransactionCoordinatorTests.swift
+++ TrustTests/Coordinators/TransactionCoordinatorTests.swift
@@ -5,35 +5,4 @@ import XCTest
 
 class TransactionCoordinatorTests: XCTestCase {
 
-    func testShowSendFlow() {
-        let coordinator = TransactionCoordinator(
-            session: .make(),
-            navigationController: FakeNavigationController(),
-            storage: FakeTransactionsStorage(),
-            keystore: FakeEtherKeystore()
-        )
-
-        coordinator.showPaymentFlow(for: .send(type: .ether(destination: .none)))
-
-        let controller = (coordinator.navigationController.presentedViewController as? UINavigationController)?.viewControllers[0]
-
-        XCTAssertTrue(coordinator.coordinators.first is PaymentCoordinator)
-        XCTAssertTrue(controller is SendViewController)
-    }
-
-    func testShowRequstFlow() {
-        let coordinator = TransactionCoordinator(
-            session: .make(),
-            navigationController: FakeNavigationController(),
-            storage: FakeTransactionsStorage(),
-            keystore: FakeEtherKeystore()
-        )
-
-        coordinator.showPaymentFlow(for: .request)
-
-        let controller = (coordinator.navigationController.presentedViewController as? UINavigationController)?.viewControllers[0]
-
-        XCTAssertTrue(coordinator.coordinators.first is PaymentCoordinator)
-        XCTAssertTrue(controller is RequestViewController)
-    }
 }
