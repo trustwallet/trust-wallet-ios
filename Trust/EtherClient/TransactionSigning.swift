cac065cf9c3ba891388768ef5af13ca743c051e8
Alejandro Isaza <al@isaza.ca>
2018-01-07 11:55:33 -0800

    Add transaction signing

---
+++ Trust/EtherClient/TransactionSigning.swift
@@ -0,0 +1,49 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import BigInt
+import CryptoSwift
+
+protocol Signer {
+    func hash(transaction: SignTransaction) -> Data
+}
+
+struct EIP155Signer: Signer {
+    let chainId: BigInt
+
+    init(chainId: BigInt) {
+        self.chainId = chainId
+    }
+
+    func hash(transaction: SignTransaction) -> Data {
+        return rlpHash([
+            transaction.nonce,
+            transaction.gasPrice,
+            transaction.gasLimit,
+            Data(hex: transaction.address.address),
+            transaction.value,
+            transaction.data,
+            transaction.chainID, 0, 0,
+        ])!
+    }
+}
+
+struct HomesteadSigner: Signer {
+    func hash(transaction: SignTransaction) -> Data {
+        return rlpHash([
+            transaction.nonce,
+            transaction.gasPrice,
+            transaction.gasLimit,
+            Data(hex: transaction.address.address),
+            transaction.value,
+            transaction.data,
+        ])!
+    }
+}
+
+func rlpHash(_ element: Any) -> Data? {
+    let sha3 = SHA3(variant: .keccak256)
+    guard let data = RLP.encode(element) else {
+        return nil
+    }
+    return Data(bytes: sha3.calculate(for: data.bytes))
+}
