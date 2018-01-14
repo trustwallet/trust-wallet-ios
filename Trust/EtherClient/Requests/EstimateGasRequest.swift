80395273250f573254aad5b83b95bfd2a61b9ad6
Kamuela Franco <kamuela.franco@gmail.com>
2018-01-14 06:52:19 +0000

    EstimateGas Feature (#210)
    * Added params parsing support to QR parser
    
    * Compiler error fixes
    
    * Bug fixes to QRURLParser
    
    * Added EstimateGasRequest.swift
    
    * Added EstimateGasRequest.swift
    
    * Fix typo in comment
    
    * Fixed typo

---
+++ Trust/EtherClient/Requests/EstimateGasRequest.swift
@@ -0,0 +1,28 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import JSONRPCKit
+
+struct EstimateGasRequest: JSONRPCKit.Request {
+    typealias Response = String
+    
+    let to: String
+    let data: String
+    
+    var method: String {
+        return "eth_estimateGas"
+    }
+    
+    var parameters: Any? {
+        return [["to": to, "data": data], "latest"]
+    }
+    
+    func response(from resultObject: Any) throws -> Response {
+        if let response = resultObject as? Response {
+            return response
+        } else {
+            throw CastError(actualValue: resultObject, expectedType: Response.self)
+        }
+    }
+}
+
