7dd92b2d0625271017fd08ac6bf1a1f0eb7f8552
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-01-16 16:33:30 +0200

    Update of the unit tests.

--- TrustTests/Factories/WalletSession.swift
+++ TrustTests/Factories/WalletSession.swift
@@ -7,11 +7,15 @@ import TrustKeystore
 extension WalletSession {
     static func make(
         account: Wallet = .make(),
-        config: Config = .make()
+        config: Config = .make(),
+        web3: Web3Swift = Web3Swift()
     ) -> WalletSession {
+        let balance =  BalanceCoordinator(account: account, storage: FakeTokensDataStore())
         return WalletSession(
             account: account,
-            config: config
+            config: config,
+            web3: web3,
+            balanceCoordinator: balance
         )
     }
 }
