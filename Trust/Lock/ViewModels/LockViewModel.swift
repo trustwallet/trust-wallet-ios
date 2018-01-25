04772ea3014339f6fe27f607e794809c96cc68e8
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-01-25 22:55:46 +0200

    Add animation and state text.

--- Trust/Lock/ViewModels/LockViewModel.swift
+++ Trust/Lock/ViewModels/LockViewModel.swift
@@ -3,6 +3,11 @@
 import UIKit
 
 class LockViewModel {
-    var lockTitle = "Enter a new password"
+    let createPasscodeinitialLabelText = "Enter a new password"
+    let createPasscodeconfirmLabelText = "Please re-enter your passcode"
+    let createPasscodemismatchedLabelText = "Passcodes did not match. Try again"
+    let enterPasscodeInitialLabelText = "Enter your passcode"
+    let enterPasscodeIncorrectLabelText = "Incorrect passcode. Try again."
+    let enterPasscodeViewControllerTitle = "Enter Passcode"
     var charCount = 6
 }
