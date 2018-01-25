5698162ce161c617504064b88cf3c6ebb09b5057
Michael Scoff <thetrustwallet@gmail.com>
2018-01-24 20:52:33 -0800

    Move SendTransaction into separate file

---
+++ Trust/Browser/Types/SendTransaction.swift
@@ -0,0 +1,13 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+
+struct SendTransaction: Decodable {
+    let from: String
+    let to: String?
+    let value: String?
+    let gas: String?
+    let gasPrice: String?
+    let data: String?
+    let nonce: String?
+}
