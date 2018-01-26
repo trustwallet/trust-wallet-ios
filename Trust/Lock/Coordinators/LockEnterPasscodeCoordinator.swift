8885120351a083c559e244c496baedf664b74c77
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-01-26 18:00:09 +0200

    Add coordinator to show passcode

---
+++ Trust/Lock/Coordinators/LockEnterPasscodeCoordinator.swift
@@ -0,0 +1,23 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+class LockEnterPasscodeCoordinator: Coordinator {
+    var coordinators: [Coordinator] = []
+    private let window: UIWindow
+    private let model: LockEnterPasscodeViewModel
+    private lazy var lockEnterPasscodeViewController: LockEnterPasscodeViewController = {
+        return LockEnterPasscodeViewController(model: model)
+    }()
+    init(window: UIWindow, model: LockEnterPasscodeViewModel) {
+        self.window = window
+        self.model = model
+    }
+    func start() {
+        window.rootViewController = lockEnterPasscodeViewController
+        window.makeKeyAndVisible()
+    }
+    func dismiss() {
+        window.isHidden = true
+    }
+}
