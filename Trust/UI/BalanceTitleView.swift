1f71b24c940608285520a03dc768f6f886fa7ded
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-02-07 00:25:09 +0200

    Add method to refresh only eth balance on very beginning. (#288)

--- Trust/UI/BalanceTitleView.swift
+++ Trust/UI/BalanceTitleView.swift
@@ -107,6 +107,7 @@ extension BalanceTitleView {
         case .token(let token):
             view.viewModel = BalanceTokenViewModel(token: token)
         }
+        session.refresh(.ethBalance)
         return view
     }
 }
