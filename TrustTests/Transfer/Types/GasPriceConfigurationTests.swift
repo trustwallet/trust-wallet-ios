926ad89d9b7bde4cf552d0ee11ca9c19c2922e60
Viktor Radchenko <yazexel@gmail.com>
2018-02-11 16:37:55 -0800

    Reduce max gas limit and gas price for easier sliding

--- TrustTests/Transfer/Types/GasPriceConfigurationTests.swift
+++ TrustTests/Transfer/Types/GasPriceConfigurationTests.swift
@@ -9,6 +9,6 @@ class GasPriceConfigurationTests: XCTestCase {
     func testDefault() {
         XCTAssertEqual(BigInt(24000000000), GasPriceConfiguration.default)
         XCTAssertEqual(BigInt(1000000000), GasPriceConfiguration.min)
-        XCTAssertEqual(BigInt(250000000000), GasPriceConfiguration.max)
+        XCTAssertEqual(BigInt(10000000000), GasPriceConfiguration.max)
     }
 }
