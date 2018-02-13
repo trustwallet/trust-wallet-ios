c0a8b77b76557786360a971f76d6e75aec5996d0
Viktor Radchenko <yazexel@gmail.com>
2018-02-12 20:24:24 -0800

    password => passcode

--- Trust/Lock/ViewModels/LockCreatePasscodeViewModel.swift
+++ Trust/Lock/ViewModels/LockCreatePasscodeViewModel.swift
@@ -4,6 +4,6 @@ import UIKit
 
 class LockCreatePasscodeViewModel: LockViewModel {
     let title = NSLocalizedString("lock.create.passcode.view.model.title", value: "Set Passcode", comment: "")
-    let initialLabelText = NSLocalizedString("lock.create.passcode.view.model.initial", value: "Enter a new password", comment: "")
+    let initialLabelText = NSLocalizedString("lock.create.passcode.view.model.initial", value: "Enter a new passcode", comment: "")
     let confirmLabelText = NSLocalizedString("lock.create.passcode.view.model.confirm", value: "Please re-enter your passcode", comment: "")
 }
