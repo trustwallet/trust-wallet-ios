a876f7668bf4a044dddbcd9b953dd84b91a37acc
Tyler Miller <tyemiller4@gmail.com>
2018-02-17 15:23:36 -0500

    add corrdinator file

---
+++ Trust/Export/Coordinators/ExportPrivateKeyCoordinator.swift
@@ -0,0 +1,18 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import Foundation
+import UIKit
+import Result
+import TrustKeystore
+
+protocol ExportPrivateKeyCoordinatorDelegate: class {
+    func didCancel(coordinator: ExportPrivateKeyCoordinator)
+    func didFinish(account: Account, in coordinator: ExportPrivateKeyCoordinator)
+}
+
+class ExportPrivateKeyCoordinator: Coordinator {
+
+    let 
+
+
+}
