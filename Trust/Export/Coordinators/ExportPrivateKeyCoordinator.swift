f38307bf040d15154277eded22957b68e9642ddc
Tyler Miller <tyemiller4@gmail.com>
2018-02-18 06:03:04 -0500

    set up coordinator

--- Trust/Export/Coordinators/ExportPrivateKeyCoordinator.swift
+++ Trust/Export/Coordinators/ExportPrivateKeyCoordinator.swift
@@ -2,17 +2,55 @@
 
 import Foundation
 import UIKit
-import Result
 import TrustKeystore
 
+
 protocol ExportPrivateKeyCoordinatorDelegate: class {
-    func didCancel(coordinator: ExportPrivateKeyCoordinator)
-    func didFinish(account: Account, in coordinator: ExportPrivateKeyCoordinator)
+    func didCancel(in coordinator: ExportPrivateKeyCoordinator)
 }
 
 class ExportPrivateKeyCoordinator: Coordinator {
 
-    let 
+    let navigationController: UINavigationController
+    weak var delegate: ExportPrivateKeyCoordinatorDelegate?
+    let keystore: Keystore
+    let account: Account
+    var coordinators: [Coordinator] = []
+    lazy var exportViewController: ExportPrivateKeyViewConroller = {
+        return self.makeExportViewController()
+    }()
+    private lazy var viewModel: ExportPrivateKeyViewModel = {
+        return .init(keystore: keystore,
+                     accout: account)
+    }()
 
+    
+    init(
+        navigationController: UINavigationController,
+        keystore: Keystore,
+        account: Account
+        ) {
+        self.navigationController = navigationController
+        self.navigationController.modalPresentationStyle = .formSheet
+        self.keystore = keystore
+        self.account = account
+    }
 
+    func start() {
+        navigationController.viewControllers = [exportViewController]
+    }
+
+    func makeExportViewController() -> ExportPrivateKeyViewConroller {
+        let controller = ExportPrivateKeyViewConroller(viewModel: viewModel)
+
+        controller.navigationItem.title = String(format: NSLocalizedString("export.PrivateKey.nav.title", value: "Export Private Key", comment: ""))
+        controller.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismiss))
+
+        return controller
+    }
+
+    @objc func dismiss() {
+        delegate?.didCancel(in: self)
+    }
 }
+
