0a226cc1ca99cf111a99360537f71cd8dd629822
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-02-23 23:18:47 +0200

    Add test to validate adress.

---
+++ TrustTests/Foundation/CryptoAddressValidatorTest.swift
@@ -0,0 +1,19 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+@testable import Trust
+import XCTest
+
+class CryptoAddressValidatorTest: XCTestCase {
+    func testValidUpperCaseEthAddress() {
+        XCTAssertEqual(true, CryptoAddressValidator.isValidAddress("0x5E31A998dA1c1371FA7dC80dA50EBC500F59094f"))
+    }
+    func testValidLoverCaseEthAddress() {
+        XCTAssertEqual(true, CryptoAddressValidator.isValidAddress("0xfa52274dd61e1643d2205169732f29114bc240b3"))
+    }
+    func testInvalidEthAddresses() {
+        XCTAssertEqual(false, CryptoAddressValidator.isValidAddress("0x0000000000000000000000000000000000000009x"))
+        XCTAssertEqual(false, CryptoAddressValidator.isValidAddress("1"))
+        XCTAssertEqual(false, CryptoAddressValidator.isValidAddress(nil))
+        XCTAssertEqual(false, CryptoAddressValidator.isValidAddress("1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v"))
+    }
+}
