4c5586057e3e7c0d07d4c414dc158741545f50a7
Viktor Radchenko <yazexel@gmail.com>
2018-02-23 21:09:28 -0800

    Pass optional token to avoid optional values

--- Trust/Tokens/ViewModels/NewTokenViewModel.swift
+++ Trust/Tokens/ViewModels/NewTokenViewModel.swift
@@ -3,15 +3,35 @@
 import Foundation
 
 struct NewTokenViewModel {
-    let isEditing: Bool
-    init(isEditing: Bool = false) {
-        self.isEditing = isEditing
+    let token: ERC20Token?
+
+    init(token: ERC20Token?) {
+        self.token = token
     }
+
     var title: String {
-        if isEditing {
+        switch token {
+        case .some:
             return NSLocalizedString("tokens.token.edit.navigation.title", value: "Edit Custom Token", comment: "")
-        } else {
+        case .none:
             return NSLocalizedString("tokens.newtoken.navigation.title", value: "Add Custom Token", comment: "")
         }
+    }
+
+    var contract: String {
+        return token?.contract.description ?? ""
+    }
+
+    var name: String {
+        return token?.name ?? ""
+    }
+
+    var symbol: String {
+        return token?.symbol ?? ""
+    }
+
+    var decimals: String {
+        guard let decimals = token?.decimals else { return "" }
+        return "\(decimals)"
     }
 }
