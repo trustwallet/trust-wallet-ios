e313046206e6f718b8801215986665533fd70d9b
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-03-02 00:12:43 +0200

    Add unit test to cover tokens view model.

--- TrustTests/Factories/FakeTokensNetwork.swift
+++ TrustTests/Factories/FakeTokensNetwork.swift
@@ -3,6 +3,7 @@
 import Foundation
 import Moya
 @testable import Trust
+import BigInt
 
 class FakeTokensNetwork: TokensNetworkProtocol {
     var provider: MoyaProvider<TrustService>
@@ -21,15 +22,18 @@ class FakeTokensNetwork: TokensNetworkProtocol {
     }
     
     func tickers(for tokens: [TokenObject], completion: @escaping ([CoinTicker]?) -> Void) {
-        //Implement mock
+        let eth_contract = "0x0000000000000000000000000000000000000000"
+        let rate = Rate(code: "ETH", price: 100.0, contract: eth_contract)
+        let ticker = CoinTicker(id: "ethereum", symbol: "ETH", price: "800", percent_change_24h: "-2.39", contract: eth_contract, image: "https://files.coinmarketcap.com/static/img/coins/128x128/ethereum.png", rate: CurrencyRate(currency: "USD", rates: [rate]))
+        completion([ticker])
     }
     
     func ethBalance(completion: @escaping (Balance?) -> Void) {
-        //Implement mock
+        completion(Balance(value: BigInt(100)))
     }
     
     func tokenBalance(for token: TokenObject, completion: @escaping ((TokenObject, Balance?)) -> Void) {
-        //Implement mock
+        completion((token, Balance(value: BigInt(400))))
     }
     
 }
--- TrustTests/Tokens/TokensViewModelTest.swift
+++ TrustTests/Tokens/TokensViewModelTest.swift
@@ -2,29 +2,36 @@
 
 import XCTest
 @testable import Trust
+import BigInt
 
 class TokensViewModelTest: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let model = TokensViewModel(realmDataStore: FakeTokensDataStore(), tokensNetwork: FakeTokensNetwork(provider: TrustProviderFactory.makeProvider(), balanceService: FakeGetBalanceCoordinator(), account: .make(), config: .make()))
+    let firstItem = IndexPath(row: 0, section: 0)
+    func testNumberOfTokens() {
+        XCTAssertEqual(1, model.numberOfItems(for: 0))
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testItem() {
+        let item = model.item(for: firstItem)
+        XCTAssertNotNil(item)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCellViewModel() {
+        let item = model.cellViewModel(for: firstItem)
+        let token = model.tokens[firstItem.row]
+        XCTAssertEqual(item.token, token)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCanEdit() {
+         XCTAssertEqual(false, model.canEdit(for: firstItem))
     }
-    
+    func testUpdateTicker() {
+        model.updateTickers()
+        let ethTicker = model.realmDataStore.tickers.first
+        XCTAssertNotNil(ethTicker)
+        XCTAssertEqual("800", ethTicker?.price)
+    }
+    func testBalance() {
+        model.updateEthBalance()
+        let token = model.tokens[firstItem.row]
+        XCTAssertNotNil(token)
+        XCTAssertEqual(BigInt(100), token.valueBigInt)
+    }
 }
