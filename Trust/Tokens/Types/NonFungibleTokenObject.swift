72f692b7b179f0a44357c8181c299fdd6bcfc708
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-03-02 00:58:29 +0200

    Add NonFungibleTokenObject fields. Extend TokensDataStore to support new operations.

--- Trust/Tokens/Types/NonFungibleTokenObject.swift
+++ Trust/Tokens/Types/NonFungibleTokenObject.swift
@@ -4,5 +4,9 @@ import RealmSwift
 import BigInt
 
 class NonFungibleTokenObject: Object {
-
+    @objc dynamic var type: String = ""
+    @objc dynamic var name: String = ""
+    @objc dynamic var annotation: String = ""
+    @objc dynamic var imagePath: String = ""
+    @objc dynamic var externalPath: String = ""
 }
