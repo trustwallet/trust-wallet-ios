8fe461365f62d158d682810b17a9d2c8ce241854
Viktor Radchenko <yazexel@gmail.com>
2018-03-03 13:04:00 -0800

    Enable Callisto

--- Trust/Settings/ViewModels/SettingsViewModel.swift
+++ Trust/Settings/ViewModels/SettingsViewModel.swift
@@ -17,7 +17,7 @@ struct SettingsViewModel {
             RPCServer.main,
             RPCServer.classic,
             RPCServer.poa,
-            // RPCServer.callisto, TODO: Enable.
+            RPCServer.callisto,
             RPCServer.kovan,
             RPCServer.ropsten,
             RPCServer.rinkeby,
