c4f2e0e580aef5c463b8c156b5a624969280ab79
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-03-04 23:29:08 +0200

    Show hide navigation buttons.

--- Trust/Tokens/ViewControllers/MasterViewController.swift
+++ Trust/Tokens/ViewControllers/MasterViewController.swift
@@ -1,10 +1,12 @@
 // Copyright SIX DAY LLC. All rights reserved.
 
 import UIKit
+
 enum DeteilsViewType: Int {
     case tokens
     case nonFungibleTokens
 }
+
 class MasterViewController: UIViewController {
     fileprivate lazy var segmentController: UISegmentedControl = {
         let items = [NSLocalizedString("Tokens", value: "Tokens", comment: ""), NSLocalizedString("Collectable", value: "Collectable", comment: "")]
@@ -13,8 +15,11 @@ class MasterViewController: UIViewController {
         segmentController.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
         return segmentController
     }()
+
     var tokensViewController: TokensViewController
+
     var nonFungibleTokensViewController: NonFungibleTokensViewController
+
     init(
         tokensViewController: TokensViewController,
         nonFungibleTokensViewController: NonFungibleTokensViewController
@@ -23,29 +28,37 @@ class MasterViewController: UIViewController {
         self.nonFungibleTokensViewController = nonFungibleTokensViewController
         super.init(nibName: nil, bundle: nil)
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.titleView = segmentController
         setupView()
     }
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
     private func setupView() {
         updateView()
     }
+
     private func updateView() {
         if segmentController.selectedSegmentIndex == DeteilsViewType.tokens.rawValue {
+            showBarButtonItems()
             remove(asChildViewController: nonFungibleTokensViewController)
             add(asChildViewController: tokensViewController)
         } else {
+            hideBarButtonItems()
             remove(asChildViewController: tokensViewController)
             add(asChildViewController: nonFungibleTokensViewController)
         }
     }
+
     @objc func selectionDidChange(_ sender: UISegmentedControl) {
         updateView()
     }
+
     private func add(asChildViewController viewController: UIViewController) {
         addChildViewController(viewController)
         view.addSubview(viewController.view)
@@ -53,9 +66,24 @@ class MasterViewController: UIViewController {
         viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         viewController.didMove(toParentViewController: self)
     }
+
     private func remove(asChildViewController viewController: UIViewController) {
         viewController.willMove(toParentViewController: nil)
         viewController.view.removeFromSuperview()
         viewController.removeFromParentViewController()
+    }
+
+    private func showBarButtonItems() {
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
+        self.navigationItem.rightBarButtonItem?.isEnabled = true
+        self.navigationItem.leftBarButtonItem?.isEnabled = true
+    }
+
+    private func hideBarButtonItems() {
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.clear
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.clear
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
+        self.navigationItem.leftBarButtonItem?.isEnabled = false
     }
 }
