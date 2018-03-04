2c61b1bfef4772cdbe118b8e4d53228949c6f62b
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-03-05 00:34:09 +0200

    Cell configuration.

--- Trust/Tokens/Views/NonFungibleTokenViewCell.swift
+++ Trust/Tokens/Views/NonFungibleTokenViewCell.swift
@@ -4,19 +4,19 @@ import UIKit
 import Kingfisher
 
 class NonFungibleTokenViewCell: UITableViewCell {
-    
+
     static let identifier = "NonFungibleTokenViewCell"
 
     @IBOutlet weak var tokenImageView: UIImageView!
-    
+
     @IBOutlet weak var annotationTextLabel: UILabel!
-    
+
     @IBOutlet weak var nameTextLabel: UILabel!
 
     func configure(viewModel: NonFungibleTokenCellViewModel) {
         tokenImageView.kf.setImage(
             with: viewModel.imagePath,
-            placeholder: nil
+            placeholder: UIImage(named: "launch_screen_logo")
         )
         nameTextLabel.text = viewModel.name
         annotationTextLabel.text = viewModel.annotation
