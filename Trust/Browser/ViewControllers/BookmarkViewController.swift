6b5ee02ace697e1feb5d2c8e539bd855f22f5ae1
Valtteri Koskivuori <vkoskiv@gmail.com>
2018-03-06 23:39:56 +0200

    Remove `numberOfSections()` in `BookmarkViewController`

--- Trust/Browser/ViewControllers/BookmarkViewController.swift
+++ Trust/Browser/ViewControllers/BookmarkViewController.swift
@@ -55,10 +55,6 @@ class BookmarkViewController: UITableViewController {
         return viewModel.bookmarks[indexPath.row]
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.bookmarks.count
     }
