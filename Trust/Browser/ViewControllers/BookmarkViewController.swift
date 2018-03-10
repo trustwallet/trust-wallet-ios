4da1ba69e3a00bc5e0502c662b0de39b227a2b1e
Valtteri Koskivuori <vkoskiv@gmail.com>
2018-03-10 12:09:14 +0200

    Change BookmarkViewController from `UITableViewController` -> `UIViewController` Implement bookmarks `UITableView` programmatically Add new `BookmarksEmptyView`

--- Trust/Browser/ViewControllers/BookmarkViewController.swift
+++ Trust/Browser/ViewControllers/BookmarkViewController.swift
@@ -3,12 +3,16 @@
 import Foundation
 import UIKit
 import Kingfisher
+import StatefulViewController
 
 protocol BookmarkViewControllerDelegate: class {
     func didSelectBookmark(_ bookmark: Bookmark, in viewController: BookmarkViewController)
 }
 
-class BookmarkViewController: UITableViewController {
+class BookmarkViewController: UIViewController {
+
+    let tableView = UITableView(frame: .zero, style: .plain)
+
     weak var delegate: BookmarkViewControllerDelegate?
 
     var viewModel: BookmarksViewModel {
@@ -31,6 +35,23 @@ class BookmarkViewController: UITableViewController {
     ) {
         self.store = store
         super.init(nibName: nil, bundle: nil)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.rowHeight = 55
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+
+        emptyView = BookmarksEmptyView()
     }
 
     override func viewDidLoad() {
@@ -43,6 +64,7 @@ class BookmarkViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetch()
+        setupInitialViewState()
     }
 
     func fetch() {
@@ -54,37 +76,7 @@ class BookmarkViewController: UITableViewController {
     }
 
     func bookmark(for indexPath: IndexPath) -> Bookmark {
-        return viewModel.bookmarks[indexPath.row]
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.bookmarks.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> BookmarkViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: R.nib.bookmarkViewCell.name, for: indexPath) as! BookmarkViewCell
-        cell.viewModel = BookmarkViewModel(bookmark: viewModel.bookmarks[indexPath.row])
-        cell.faviconImage?.kf.setImage(
-            with: cell.viewModel?.imageURL,
-            placeholder: R.image.launch_screen_logo()
-        )
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let bookmark = self.bookmark(for: indexPath)
-            confirmDelete(bookmark: bookmark, index: indexPath)
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let bookmark = self.bookmark(for: indexPath)
-        delegate?.didSelectBookmark(bookmark, in: self)
+        return bookmarks[indexPath.row]
     }
 
     func confirmDelete(bookmark: Bookmark, index: IndexPath) {
@@ -106,5 +98,49 @@ class BookmarkViewController: UITableViewController {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BookmarkViewController: StatefulViewController {
+    func hasContent() -> Bool {
+        return viewModel.hasBookmarks
+    }
+}
+
+extension BookmarkViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.bookmarks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: R.nib.bookmarkViewCell.name, for: indexPath) as! BookmarkViewCell
+        cell.viewModel = BookmarkViewModel(bookmark: viewModel.bookmarks[indexPath.row])
+        cell.faviconImage?.kf.setImage(
+            with: cell.viewModel?.imageURL,
+            placeholder: R.image.launch_screen_logo()
+        )
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return StyleLayout.TableView.heightForHeaderInSection
+    }
+}
+
+extension BookmarkViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let bookmark = self.bookmark(for: indexPath)
+            confirmDelete(bookmark: bookmark, index: indexPath)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let bookmark = self.bookmark(for: indexPath)
+        delegate?.didSelectBookmark(bookmark, in: self)
     }
 }
