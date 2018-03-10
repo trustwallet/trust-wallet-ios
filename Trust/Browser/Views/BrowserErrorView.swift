f813061af63e6fb2ce14e7e3ba8919adf4b14d37
Tao Xu <hewigovens@gmail.com>
2018-03-10 08:36:05 +0800

    Add BrowserErrorView (#434)
    * Fix #363 Add BrowserErrorView
    
    * Rebase master
    
    * Add reload button

---
+++ Trust/Browser/Views/BrowserErrorView.swift
@@ -0,0 +1,73 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+protocol BrowserErrorViewDelegate: class {
+    func didTapReload(_ sender: Button)
+}
+
+class BrowserErrorView: UIView {
+
+    weak var delegate: BrowserErrorViewDelegate?
+
+    private let topMargin: CGFloat = 120
+    private let leftMargin: CGFloat = 40
+    private let buttonTopMargin: CGFloat = 6
+
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = Colors.gray
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var reloadButton: Button = {
+        let button = Button(size: .normal, style: .borderless)
+        button.addTarget(self, action: #selector(reloadTapped), for: .touchUpInside)
+        button.setTitle(NSLocalizedString("browser.reload.button.title", value: "Reload", comment: ""), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.sizeToFit()
+        return button
+    }()
+
+    init() {
+        super.init(frame: CGRect.zero)
+        finishInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        finishInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func show(error: Error) {
+        self.isHidden = false
+        textLabel.text = error.localizedDescription
+        textLabel.textAlignment = .center
+        textLabel.setNeedsLayout()
+    }
+
+    @objc func reloadTapped() {
+        delegate?.didTapReload(reloadButton)
+    }
+
+    private func finishInit() {
+        self.backgroundColor = .white
+        addSubview(textLabel)
+        addSubview(reloadButton)
+        NSLayoutConstraint.activate([
+            textLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: leftMargin),
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -leftMargin),
+            textLabel.topAnchor.constraint(equalTo: topAnchor, constant: topMargin),
+
+            reloadButton.centerXAnchor.constraint(equalTo: textLabel.centerXAnchor),
+            reloadButton.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: buttonTopMargin),
+        ])
+    }
+}
