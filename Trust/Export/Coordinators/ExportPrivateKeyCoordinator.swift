9cb46dd9dff1da4d672113a12aa7340d07e36239
Tyler Miller <tyemiller4@gmail.com>
2018-03-15 18:29:58 -0400

    Fix export dismissal

--- Trust/Export/Coordinators/ExportPrivateKeyCoordinator.swift
+++ Trust/Export/Coordinators/ExportPrivateKeyCoordinator.swift
@@ -35,7 +35,6 @@ class ExportPrivateKeyCoordinator: Coordinator {
     }
 
     func start() {
-        //navigationController.viewControllers = [exportViewController]
         navigationController.pushViewController(exportViewController, animated: false)
     }
 
