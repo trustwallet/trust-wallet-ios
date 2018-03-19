85bf2ab833785bd54feb8cfea112a3eaba6cdb4d
Viktor Radchenko <yazexel@gmail.com>
2018-03-18 19:55:42 -0700

    Change Realm file naming for the db.

--- Trust/Core/Types/TrustRealmConfiguration.swift
+++ Trust/Core/Types/TrustRealmConfiguration.swift
@@ -8,7 +8,20 @@ struct RealmConfiguration {
 
     static func configuration(for account: Wallet, chainID: Int) -> Realm.Configuration {
         var config = Realm.Configuration()
-        config.fileURL = config.fileURL!.deletingLastPathComponent().appendingPathComponent("\(account.address.description.lowercased())-\(chainID).realm")
+        let directory = config.fileURL!.deletingLastPathComponent()
+        let oldURL = directory.appendingPathComponent("\(account.address.description.lowercased())-\(chainID).realm")
+        let newURL = directory.appendingPathComponent("\(account.description)-\(chainID).realm")
+        let fileManager = FileManager.default
+        // Moving old users to the new realm db naming. Before if you had watch/private key for the same address it would share data between them, now it's split into separaate types.
+        if fileManager.fileExists(atPath: oldURL.path) {
+            do {
+               try fileManager.moveItem(atPath: oldURL.path, toPath: newURL.path)
+            } catch {
+                config.fileURL = oldURL
+                return config
+            }
+        }
+        config.fileURL = newURL
         return config
     }
 }
