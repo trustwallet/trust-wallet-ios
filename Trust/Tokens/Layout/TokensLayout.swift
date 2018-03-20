465400da5c23d316e47b78cca34a12bc1fdb792e
Viktor Radchenko <yazexel@gmail.com>
2018-03-20 02:57:40 -0700

    Reduce cell height

--- Trust/Tokens/Layout/TokensLayout.swift
+++ Trust/Tokens/Layout/TokensLayout.swift
@@ -5,7 +5,7 @@ import UIKit
 
 struct TokensLayout {
     struct tableView {
-        static let height: CGFloat = 94
+        static let height: CGFloat = 88
         static let separatorColor = UIColor(hex: "d7d7d7")
         static let layoutInsets = UIEdgeInsets(top: 0, left: 86, bottom: 0, right: 0)
     }
