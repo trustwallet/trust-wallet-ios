8ee9e2a38f728a392efb163aaef5948fe261253b
Tyler Miller <tyemiller4@gmail.com>
2018-03-26 15:03:30 -0400

    Add Spotify like tabBar Animation

--- Trust/UI/TabBarController.swift
+++ Trust/UI/TabBarController.swift
@@ -4,5 +4,14 @@ import Foundation
 import UIKit
 
 class TabBarController: UITabBarController {
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let tabBarButtonView = item.value(forKey: "view") as? UIView else { return }
 
+        let animationDuration: TimeInterval = 0.4
+        let propertyAnimator = UIViewPropertyAnimator(duration: animationDuration, dampingRatio: 0.5) {
+            tabBarButtonView.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+        }
+        propertyAnimator.addAnimations({ tabBarButtonView.transform = .identity }, delayFactor: CGFloat(animationDuration))
+        propertyAnimator.startAnimation()
+    }
 }
