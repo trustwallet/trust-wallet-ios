5c2ee4c2d445f877ea74baeaf826fcd8c779f20b
Tyler Miller <tyemiller4@gmail.com>
2018-04-03 22:17:47 -0400

    remove unnecessary if statement

--- Trust/UI/ActivityViewController.swift
+++ Trust/UI/ActivityViewController.swift
@@ -3,22 +3,20 @@
 import UIKit
 
 class ActivityViewController: UIActivityViewController {
-     func setCompletion(navigation: UINavigationController) {
-        self.completionWithItemsHandler = { _, completed, _, _ in
-            if completed || !completed {
-                // Set the tint globally via appearance proxy
-                UINavigationBar.appearance().barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
-                UINavigationBar.appearance().titleTextAttributes = [
-                    .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
-                ]
-                navigation.navigationBar.barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
-                navigation.navigationBar.titleTextAttributes = [
-                    .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
-                ] // Used to manually set the navBar tint/text back look at this for more info on why https://stackoverflow.com/a/21653004
-            }
+    func setCompletion(navigation: UINavigationController) {
+        self.completionWithItemsHandler = { _, _, _, _ in
+            // Set the tint globally via appearance proxy
+            UINavigationBar.appearance().barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
+            UINavigationBar.appearance().titleTextAttributes = [
+                .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
+            ]
+            navigation.navigationBar.barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
+            navigation.navigationBar.titleTextAttributes = [
+                .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
+            ] // Used to manually set the navBar tint/text back look at this for more info on why https://stackoverflow.com/a/21653004
         }
     }
-   override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         UINavigationBar.appearance().barTintColor = nil // This fixes the issue with notes
         UINavigationBar.appearance().titleTextAttributes = nil // This makes the text black for messages and mail
     }
