9c43f86862f091cd30372d8b570344abeef1f57c
Viktor Radchenko <yazexel@gmail.com>
2018-04-10 20:54:36 -0700

    Move from completionWithItemsHandler to viewDidDisappear (#595)
    * Move from completionWithItemsHandler to viewDidDisappear
    
    * Remove changing status bar

--- Trust/UI/ActivityViewController.swift
+++ Trust/UI/ActivityViewController.swift
@@ -3,27 +3,12 @@
 import UIKit
 
 class ActivityViewController: UIActivityViewController {
-    override func viewWillAppear(_ animated: Bool) {
-        UINavigationBar.appearance().barTintColor = nil // This fixes the issue with notes
-        UINavigationBar.appearance().titleTextAttributes = nil // This makes the text black for messages and mail
+
+    override init(activityItems: [Any], applicationActivities: [UIActivity]?) {
+        super.init(activityItems: activityItems, applicationActivities: applicationActivities)
     }
 
-    init(activityItems: [Any], applicationActivities: [UIActivity]?, navigation: UINavigationController) {
-        super.init(activityItems: activityItems, applicationActivities: applicationActivities)
-        // Set a default completion
-        self.completionWithItemsHandler = { _, _, _, _ in
-            // Set the tint globally via appearance proxy
-            UINavigationBar.appearance().barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
-            UINavigationBar.appearance().titleTextAttributes = [
-                .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
-            ]
-            navigation.navigationBar.barTintColor = AppStyle.activityViewControllerNavigationBarTintColor
-            navigation.navigationBar.titleTextAttributes = [
-                .foregroundColor: AppStyle.activityViewControllerNavigationBarText,
-            ] // Used to manually set the navBar tint/text back look at this for more info on why https://stackoverflow.com/a/21653004
-        }
-    }
-    static func makeShareController(url: Any, navigationController: UINavigationController) -> ActivityViewController {
-        return ActivityViewController(activityItems: [url], applicationActivities: nil, navigation: navigationController)
+    static func makeShareController(url: Any) -> ActivityViewController {
+        return ActivityViewController(activityItems: [url], applicationActivities: nil)
     }
 }
