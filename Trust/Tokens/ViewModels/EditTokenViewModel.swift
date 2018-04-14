53d810f27bda6ec874d9e3641c33d0a6c15606e0
hewigovens <hewigovens@gmail.com>
2018-04-14 09:28:24 +0800

    Refactor token search results

--- Trust/Tokens/ViewModels/EditTokenViewModel.swift
+++ Trust/Tokens/ViewModels/EditTokenViewModel.swift
@@ -4,33 +4,21 @@ import Foundation
 import UIKit
 import Moya
 
-enum EditTokenSection: Int {
-    case search = 0
-    case local
-}
-
 class EditTokenViewModel {
 
     let network: NetworkProtocol
     let storage: TokensDataStore
     let config: Config
-    let tableView: UITableView
-    let feedbackGenerator = UINotificationFeedbackGenerator()
 
-    var searchResults = [TokenObject]()
-    var filteredTokens = [TokenObject]()
-    var isSearching = false
     var localSet = Set<TokenObject>()
 
     init(network: NetworkProtocol,
          storage: TokensDataStore,
-         config: Config,
-         table: UITableView
+         config: Config
     ) {
         self.network = network
         self.storage = storage
         self.config = config
-        self.tableView = table
 
         self.localSet = Set(storage.objects)
     }
@@ -44,82 +32,39 @@ class EditTokenViewModel {
     }
 
     var numberOfSections: Int {
-        return 2
+        return 1
     }
 
     func numberOfRowsInSection(_ section: Int) -> Int {
-        if section == EditTokenSection.search.rawValue {
-            return searchResults.count
-        } else {
-            if isSearching {
-                return filteredTokens.count
-            }
-            return storage.objects.count
-        }
+        return storage.objects.count
     }
 
     func token(for indexPath: IndexPath) -> (token: TokenObject, local: Bool) {
-        if indexPath.section == EditTokenSection.search.rawValue {
-            return (searchResults[indexPath.row], false)
-        } else {
-            if isSearching {
-                return (filteredTokens[indexPath.row], true)
+        return (storage.objects[indexPath.row], true)
+    }
+
+    func searchNetwork(token: String, completion: (([TokenObject]) -> Void)?) {
+        network.search(token: token) { [weak self] (tokens) in
+            var filterSet = Set<TokenObject>()
+            if let localSet = self?.localSet {
+                filterSet = localSet
             }
-            return (storage.objects[indexPath.row], true)
-        }
-    }
-
-    func didSelectRowAt(_ indexPath: IndexPath) -> Bool {
-        guard indexValidation(with: indexPath) else {
-            return false
-        }
-        let pair = token(for: indexPath)
-        if !pair.local {
-            storage.add(tokens: [pair.token])
-            feedbackGenerator.notificationOccurred(.success)
-            return true
-        }
-        return false
-    }
-
-    func search(token: String) {
-        isSearching = !token.isEmpty
-        if !self.isSearching {
-            searchResults.removeAll()
-            filteredTokens.removeAll()
-            tableView.reloadData()
-        } else {
-            filter(for: token)
-            network.search(token: token) { [weak self] (tokens) in
-                guard let strongSelf = self else { return }
-                tokens.forEach {
-                    $0.isCustom = true
-                    $0.isDisabled = false
-                }
-                strongSelf.searchResults = tokens.filter { !strongSelf.localSet.contains($0) }
-                strongSelf.tableView.reloadData()
+            tokens.forEach {
+                $0.isCustom = true
+                $0.isDisabled = false
             }
+            completion?(tokens.filter { !filterSet.contains($0) })
         }
     }
 
-    func filter(for searchText: String?) {
+    func searchLocal(token searchText: String?) -> [TokenObject] {
         let text = searchText?.lowercased() ?? ""
-        self.filteredTokens = storage.objects.filter { $0.name.lowercased().contains(text) || $0.symbol.lowercased().contains(text) }
+        let filteredTokens = storage.objects.filter { $0.name.lowercased().contains(text) || $0.symbol.lowercased().contains(text) }
+        return filteredTokens
     }
 
     func updateToken(indexPath: IndexPath, action: TokenAction) {
         let token = self.token(for: indexPath)
         self.storage.update(tokens: [token.token], action: action)
-    }
-
-    private func indexValidation(with path: IndexPath ) -> Bool {
-        if path.section == EditTokenSection.search.rawValue {
-            return searchResults.count > path.row
-        } else {
-            if isSearching {
-                return filteredTokens.count > path.row
-            }
-            return storage.objects.count > path.row
-        }
     }
 }
