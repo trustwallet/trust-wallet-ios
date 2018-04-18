4cd267f26a1de68ef96a3c8b741511f7fac57a95
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-04-18 17:31:22 +0300

    Add async realm database update in background thread.

--- Trust/Tokens/Storage/TokensDataStore.swift
+++ Trust/Tokens/Storage/TokensDataStore.swift
@@ -94,13 +94,28 @@ class TokensDataStore {
         }
     }
 
+    //Background update of the Realm model.
+    func update(balance: BigInt, for address: Address) {
+        if let tokenToUpdate = enabledObject.first(where: { $0.contract == address.description }) {
+            self.realm.writeAsync(obj: tokenToUpdate) { (realm, tokenToUpdate ) in
+                let update = [
+                    "contract": address.description,
+                    "value": balance.description,
+                ]
+                if tokenToUpdate?.value != balance.description {
+                    realm.create(TokenObject.self, value: update, update: true)
+                }
+            }
+        }
+    }
+
     func update(balances: [Address: BigInt]) {
         try! realm.write {
             for balance in balances {
                 let update = [
                     "contract": balance.key.description,
                     "value": balance.value.description,
-                ]
+                    ]
                 realm.create(TokenObject.self, value: update, update: true)
             }
         }
@@ -162,5 +177,26 @@ class TokensDataStore {
             value: "0",
             isCustom: false
         )
+    }
+}
+
+extension Realm {
+    func writeAsync<T : ThreadConfined>(obj: T, errorHandler: @escaping ((_ error : Swift.Error) -> Void) = { _ in return }, block: @escaping ((Realm, T?) -> Void)) {
+        let wrappedObj = ThreadSafeReference(to: obj)
+        let config = self.configuration
+        DispatchQueue(label: "background").async {
+            autoreleasepool {
+                do {
+                    let realm = try Realm(configuration: config)
+                    let obj = realm.resolve(wrappedObj)
+
+                    try realm.write {
+                        block(realm, obj)
+                    }
+                } catch {
+                    errorHandler(error)
+                }
+            }
+        }
     }
 }
