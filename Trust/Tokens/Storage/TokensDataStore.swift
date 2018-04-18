81e1d2b8c2d275e28b9f80f09dcd7e95cd7af0c2
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-04-18 23:28:05 +0300

    Add extension

--- Trust/Tokens/Storage/TokensDataStore.swift
+++ Trust/Tokens/Storage/TokensDataStore.swift
@@ -179,24 +179,3 @@ class TokensDataStore {
         )
     }
 }
-
-extension Realm {
-    func writeAsync<T : ThreadConfined>(obj: T, errorHandler: @escaping ((_ error : Swift.Error) -> Void) = { _ in return }, block: @escaping ((Realm, T?) -> Void)) {
-        let wrappedObj = ThreadSafeReference(to: obj)
-        let config = self.configuration
-        DispatchQueue(label: "background").async {
-            autoreleasepool {
-                do {
-                    let realm = try Realm(configuration: config)
-                    let obj = realm.resolve(wrappedObj)
-
-                    try realm.write {
-                        block(realm, obj)
-                    }
-                } catch {
-                    errorHandler(error)
-                }
-            }
-        }
-    }
-}
