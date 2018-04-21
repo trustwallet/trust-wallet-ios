24b9f60a363468fd89647f618dddb7e75f7f941d
Viktor Radchenko <yazexel@gmail.com>
2018-04-20 18:27:11 -0700

    Add import wallet event

--- Trust/Core/Analytics/AnalyticsEvent.swift
+++ Trust/Core/Analytics/AnalyticsEvent.swift
@@ -4,18 +4,23 @@ import Foundation
 
 enum AnalyticsEvent {
     case welcomeScreen
+    case importWallet(ImportSelectionType)
 
     var event: String {
         switch self {
         case .welcomeScreen:
             return "welcomeScreen"
+        case .importWallet:
+            return "importWallet"
         }
     }
 
-    var params: [String: AnyObject] {
+    var params: [String: Any] {
         switch self {
         case .welcomeScreen:
             return [:]
+        case .importWallet(let type):
+            return ["type": type.title]
         }
     }
 }
