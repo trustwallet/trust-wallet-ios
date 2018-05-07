6e54162290ab79c1af0969b7700809fccb9fd33b
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-05-07 20:16:29 +0300

    Add preference to disable send logs.

--- Trust/Core/Analytics/Analytics.swift
+++ Trust/Core/Analytics/Analytics.swift
@@ -5,7 +5,13 @@ import Firebase
 
 class Analytics {
 
+    static private var sendLogsAreDisabled: Bool {
+        let preferences = PreferencesController()
+        return preferences.get(for: .sendLogs)
+    }
+
     static func track(_ event: AnalyticsEvent) {
+        guard !sendLogsAreDisabled else { return }
         Firebase.Analytics.logEvent(event.event, parameters: event.params)
     }
 }
