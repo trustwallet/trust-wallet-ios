b98978c0b03cb101ad69d1ca9346b9ab6df1b3f9
Viktor Radchenko <yazexel@gmail.com>
2018-05-11 15:11:48 -0700

    Add type to PushDevice

--- Trust/Models/PushDevice.swift
+++ Trust/Models/PushDevice.swift
@@ -2,9 +2,15 @@
 
 import Foundation
 
+enum DeviceType: String, Encodable {
+    case ios
+    case android
+}
+
 struct PushDevice: Encodable {
     let deviceID: String
     let token: String
     let wallets: [String]
+    let type: DeviceType = .ios
     let preferences: Preferences
 }
