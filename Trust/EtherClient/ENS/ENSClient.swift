b227f714d1f463ae2b54cd9a59b8ed29b54fb25b
hewigovens <hewigovens@gmail.com>
2018-05-17 12:14:09 +0800

    remove hardcoded default reverse resolver

--- Trust/EtherClient/ENS/ENSClient.swift
+++ Trust/EtherClient/ENS/ENSClient.swift
@@ -24,9 +24,7 @@ typealias ENSResolveResult = (resolver: Address, address: Address)
 
 struct ENSClient {
 
-    static let ensContrct = Address(string: "0x314159265dd8dbb310642f98f50c066173c1259b")!
-    static let publicResolverContract = Address(string: "0x5fbb459c49bb06083c33109fa4f14810ec2cf358")!
-    static let reverseResolverContract = Address(string: "0x5fbb459c49bb06083c33109fa4f14810ec2cf358")!
+    static let ensContract = Address(string: "0x314159265dd8dbb310642f98f50c066173c1259b")!
     static let reverseSuffix = "addr.reverse"
 
     func resolve(name: String) -> Promise<ENSResolveResult> {
@@ -50,7 +48,7 @@ struct ENSClient {
         let node = namehash(name)
         let encoded = ENSEncoder.encodeResolver(node: node)
         let request = EtherServiceRequest(
-            batch: BatchFactory().create(CallRequest(to: ENSClient.ensContrct.description, data: encoded.hexEncoded))
+            batch: BatchFactory().create(CallRequest(to: ENSClient.ensContract.description, data: encoded.hexEncoded))
         )
         return self.sendAddr(request: request)
     }
@@ -59,9 +57,41 @@ struct ENSClient {
         let node = namehash(name)
         let encoded = ENSEncoder.encodeOwner(node: node)
         let request = EtherServiceRequest(
-            batch: BatchFactory().create(CallRequest(to: ENSClient.ensContrct.description, data: encoded.hexEncoded))
+            batch: BatchFactory().create(CallRequest(to: ENSClient.ensContract.description, data: encoded.hexEncoded))
         )
         return self.sendAddr(request: request)
+    }
+
+    func lookup(address: Address) -> Promise<String> {
+        let addr = [address.data.hex, ENSClient.reverseSuffix].joined(separator: ".")
+        return firstly {
+            return self.resolverOf(name: addr)
+        }.then { resolver -> Promise<String> in
+            if resolver == Address.zero {
+                return Promise { $0.resolve("", nil) }
+            }
+            let node = namehash(addr)
+            let encoded = ReverseResolverEncoder.encodeName(node)
+            let request = EtherServiceRequest(
+                batch: BatchFactory().create(CallRequest(to: resolver.description, data: encoded.hexEncoded))
+            )
+            return Promise { seal in
+                Session.send(request) { result in
+                    switch result {
+                    case .success(let response):
+                        let data = Data(hex: response)
+                        let decoder = ABIDecoder(data: data)
+                        let decoded = try? decoder.decodeTuple(types: [.string])
+                        guard let string = decoded?.first?.nativeValue as? String else {
+                            return seal.reject(ENSError.decodeError)
+                        }
+                        seal.fulfill(string)
+                    case .failure(let error):
+                        seal.reject(error)
+                    }
+                }
+            }
+        }
     }
 
     private func sendAddr(request: EtherServiceRequest<Batch1<CallRequest>>) -> Promise<Address> {
@@ -76,31 +106,6 @@ struct ENSClient {
                     //take the last 20 bytes for Address
                     let sub = data.suffix(20)
                     seal.fulfill(Address(data: sub))
-                case .failure(let error):
-                    seal.reject(error)
-                }
-            }
-        }
-    }
-
-    func lookup(address: Address) -> Promise<String> {
-        let addr = [address.data.hex, ENSClient.reverseSuffix].joined(separator: ".")
-        let node = namehash(addr)
-        let encoded = ReverseResolverEncoder.encodeName(node)
-        let request = EtherServiceRequest(
-            batch: BatchFactory().create(CallRequest(to: ENSClient.reverseResolverContract.description, data: encoded.hexEncoded))
-        )
-        return Promise { seal in
-            Session.send(request) { result in
-                switch result {
-                case .success(let response):
-                    let data = Data(hex: response)
-                    let decoder = ABIDecoder(data: data)
-                    let decoded = try? decoder.decodeTuple(types: [.string])
-                    guard let string = decoded?.first?.nativeValue as? String else {
-                        return seal.reject(ENSError.decodeError)
-                    }
-                    seal.fulfill(string)
                 case .failure(let error):
                     seal.reject(error)
                 }
