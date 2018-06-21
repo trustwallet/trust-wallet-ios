d0db70e9eb52c6b2edb7cb9fffe7acb0677fe894
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-06-21 18:00:08 +0300

    Disable crashltics report.

--- Trust/Core/Initializers/CrashReportInitializer.swift
+++ Trust/Core/Initializers/CrashReportInitializer.swift
@@ -4,11 +4,19 @@ import Foundation
 import Fabric
 import Crashlytics
 
-struct CrashReportInitializer: Initializer {
+class CrashReportInitializer: NSObject, Initializer {
 
     func perform() {
         guard !isDebug else { return }
-
+        Crashlytics.sharedInstance().delegate = self
         Fabric.with([Crashlytics.self, Answers.self])
+    }
+
+}
+
+extension CrashReportInitializer: CrashlyticsDelegate {
+
+    func crashlyticsDidDetectReport(forLastExecution report: CLSReport, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(Analitics.crashlytics.isEnabled)
     }
 }
