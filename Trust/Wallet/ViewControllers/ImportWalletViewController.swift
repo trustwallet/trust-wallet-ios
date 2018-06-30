7f1a5c79aa59164d1db7cc44732776dd1d20f5bf
Viktor Radchenko <yazexel@gmail.com>
2018-06-30 14:22:53 -0700

    Disable import from file

--- Trust/Wallet/ViewControllers/ImportWalletViewController.swift
+++ Trust/Wallet/ViewControllers/ImportWalletViewController.swift
@@ -56,7 +56,8 @@ class ImportWalletViewController: FormViewController {
 
         title = viewModel.title
         navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(image: R.image.import_options(), style: .done, target: self, action: #selector(importOptions)),
+            // Disabled, since iCloud entitlment is missing. Required for the transfer to the new entity
+            // UIBarButtonItem(image: R.image.import_options(), style: .done, target: self, action: #selector(importOptions)),
             UIBarButtonItem(image: R.image.qr_code_icon(), style: .done, target: self, action: #selector(openReader)),
         ]
 
