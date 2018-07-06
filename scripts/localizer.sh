a16414395f15b15f71feed363e495dea0036c15b
Viktor Radchenko <yazexel@gmail.com>
2018-07-06 14:21:00 -0700

    Move tools into scripts folder

--- scripts/localizer.sh
+++ scripts/localizer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 find $1 -name "*.swift" ! -path "$1/R.generated.swift" -print0 | xargs -0 xcrun extractLocStrings
 
-tools/lokalise --token $2 import 3947163159df13df851b51.98101647 --file Localizable.strings --lang_iso en --replace 1
-tools/lokalise --token $2 export 3947163159df13df851b51.98101647 --type strings --bundle_structure %LANG_ISO%.lproj/Localizable.%FORMAT% --export_empty skip
+scripts/tools/lokalise --token $2 import 3947163159df13df851b51.98101647 --file Localizable.strings --lang_iso en --replace 1
+scripts/tools/lokalise --token $2 export 3947163159df13df851b51.98101647 --type strings --bundle_structure %LANG_ISO%.lproj/Localizable.%FORMAT% --export_empty skip
 unzip -o Trust-Localizable.zip -d $1/Localization
