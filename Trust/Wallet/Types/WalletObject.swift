6772b0f63a0b51adaf072b349814d5ff56605025
Viktor Radchenko <yazexel@gmail.com>
2018-07-23 01:35:46 -0700

    Move WalletAddress to separate file

--- Trust/Wallet/Types/WalletObject.swift
+++ Trust/Wallet/Types/WalletObject.swift
@@ -23,39 +23,3 @@ final class WalletObject: Object {
         return info
     }
 }
-
-final class WalletAddress: Object {
-    @objc dynamic var id: String = UUID().uuidString
-    @objc dynamic var addressString: String = ""
-    @objc private dynamic var rawCoin = -1
-    public var coin: Coin {
-        get { return Coin(rawValue: rawCoin) ?? .ethereum }
-        set { rawCoin = newValue.rawValue }
-    }
-
-    convenience init(
-        coin: Coin,
-        address: Address
-    ) {
-        self.init()
-        self.addressString = address.description
-        self.coin = coin
-    }
-
-    var address: EthereumAddress? {
-        return EthereumAddress(string: addressString)
-    }
-
-    override static func primaryKey() -> String? {
-        return "id"
-    }
-
-    override static func ignoredProperties() -> [String] {
-        return ["coin"]
-    }
-
-    override func isEqual(_ object: Any?) -> Bool {
-        guard let object = object as? WalletAddress else { return false }
-        return object.address == address
-    }
-}
