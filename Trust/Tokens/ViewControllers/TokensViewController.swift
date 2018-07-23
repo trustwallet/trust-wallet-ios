7db952ebb45a2caaff4ec0763fa1a03a51c00584
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-07-23 19:25:21 +0300

    Replace injection of the cell view model in more appropriate place. (#834)

--- Trust/Tokens/ViewControllers/TokensViewController.swift
+++ Trust/Tokens/ViewControllers/TokensViewController.swift
@@ -192,13 +192,15 @@ extension TokensViewController: UITableViewDelegate {
 extension TokensViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TokenViewCell.identifier, for: indexPath) as! TokenViewCell
-        cell.configure(viewModel: viewModel.cellViewModel(for: indexPath))
         cell.isExclusiveTouch = true
         return cell
     }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.tokens.count
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+       guard let tokenViewCell = cell as? TokenViewCell else { return }
+       tokenViewCell.configure(viewModel: viewModel.cellViewModel(for: indexPath))
     }
 }
 extension TokensViewController: TokensViewModelDelegate {
