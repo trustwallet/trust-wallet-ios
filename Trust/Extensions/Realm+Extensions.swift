08776dc194c88a7c3fb2129b597e16749726837c
Oleg Gordiichuk <oleghordiichuk@gmail.com>
2018-07-25 00:56:51 +0300

    Add ability to work with Realm in background queue.

--- Trust/Extensions/Realm+Extensions.swift
+++ Trust/Extensions/Realm+Extensions.swift
@@ -3,6 +3,9 @@
 import RealmSwift
 
 extension Realm {
+
+    static let bacgroundQueue = DispatchQueue(label: "Realm",qos: .utility)
+
     func writeAsync<T: ThreadConfined>(obj: T, errorHandler: @escaping ((_ error: Swift.Error) -> Void) = { _ in return }, block: @escaping ((Realm, T?) -> Void)) {
         let wrappedObj = ThreadSafeReference(to: obj)
         let config = self.configuration
@@ -18,6 +21,16 @@ extension Realm {
                 } catch {
                     errorHandler(error)
                 }
+            }
+        }
+    }
+
+    func backgroundInstance(_ block: @escaping (Realm) -> Void, errorHandler: @escaping ((_ error: Swift.Error) -> Void) = { _ in return }) {
+        Realm.bacgroundQueue.async {
+            do {
+                block(try Realm(configuration: self.configuration))
+            } catch(let error) {
+                errorHandler(error)
             }
         }
     }
