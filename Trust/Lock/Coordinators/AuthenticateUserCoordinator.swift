a83b0cebe7ab3b970aee6438bad256681d7ed043
Viktor Radchenko <yazexel@gmail.com>
2018-07-26 16:13:06 -0700

    Add AuthenticateUserCoordinator
    TODO: work all the logic. just a mockup

---
+++ Trust/Lock/Coordinators/AuthenticateUserCoordinator.swift
@@ -0,0 +1,46 @@
+// Copyright DApps Platform Inc. All rights reserved.
+
+import Foundation
+
+final class AuthenticateUserCoordinator: Coordinator {
+
+    var coordinators: [Coordinator] = []
+    let navigationController: NavigationController
+    private let model: LockEnterPasscodeViewModel
+    private let lock: LockInterface
+    private lazy var lockEnterPasscodeViewController: LockEnterPasscodeViewController = {
+        return LockEnterPasscodeViewController(model: model)
+    }()
+
+    init(
+        navigationController: NavigationController,
+        model: LockEnterPasscodeViewModel = LockEnterPasscodeViewModel(),
+        lock: LockInterface = Lock()
+    ) {
+        self.navigationController = navigationController
+        self.model = model
+        self.lock = lock
+
+        lockEnterPasscodeViewController.unlockWithResult = { [weak self] (state, bioUnlock) in
+            if state {
+                self?.stop()
+            }
+        }
+    }
+
+    func start() {
+        guard lock.shouldShowProtection() else { return }
+
+        navigationController.present(lockEnterPasscodeViewController, animated: true)
+    }
+
+    func showAuthentication() {
+        lockEnterPasscodeViewController.showKeyboard()
+        lockEnterPasscodeViewController.showBioMerickAuth()
+        lockEnterPasscodeViewController.cleanUserInput()
+    }
+
+    func stop() {
+        navigationController.dismiss(animated: true)
+    }
+}
--- TrustTests/Lock/Coordinators/AuthenticateUserCoordinatorTests.swift
+++ TrustTests/Lock/Coordinators/AuthenticateUserCoordinatorTests.swift
@@ -1,29 +1,15 @@
 // Copyright DApps Platform Inc. All rights reserved.
 
 import XCTest
+@testable import Trust
 
 class AuthenticateUserCoordinatorTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testStart() {
+        let coordinator = AuthenticateUserCoordinator(navigationController: FakeNavigationController())
+
+        coordinator.start()
+
+        XCTAssertTrue(coordinator.navigationController.presentedViewController is LockEnterPasscodeViewController)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
