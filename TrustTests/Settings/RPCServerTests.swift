8332950ce5f223d7b74989eb2086e8ee08d538d2
Viktor Radchenko <yazexel@gmail.com>
2018-07-26 15:32:30 -0700

    Add tests to RPCServerTests

--- TrustTests/Settings/RPCServerTests.swift
+++ TrustTests/Settings/RPCServerTests.swift
@@ -2,12 +2,23 @@
 
 import XCTest
 @testable import Trust
+import TrustCore
 
 class RPCServerTests: XCTestCase {
-    
-    func testMainNetwork() {
-        let server = RPCServer(chainID: 1)
 
-        XCTAssertEqual(.main, server)
+    func testServerToCoinMapping() {
+        XCTAssertEqual(RPCServer.main.coin, Coin.ethereum)
+        XCTAssertEqual(RPCServer.poa.coin, Coin.poa)
+        XCTAssertEqual(RPCServer.classic.coin, Coin.ethereumClassic)
+        XCTAssertEqual(RPCServer.callisto.coin, Coin.callisto)
+        XCTAssertEqual(RPCServer.gochain.coin, Coin.gochain)
+    }
+
+    func testisDisabledByDefault() {
+        XCTAssertEqual(RPCServer.main.isDisabledByDefault, false)
+        XCTAssertEqual(RPCServer.poa.isDisabledByDefault, true)
+        XCTAssertEqual(RPCServer.classic.isDisabledByDefault, true)
+        XCTAssertEqual(RPCServer.callisto.isDisabledByDefault, true)
+        XCTAssertEqual(RPCServer.gochain.isDisabledByDefault, true)
     }
 }
