611a599dae244eff94e845e02a9e6bb2c1d69aff
Viktor Radchenko <yazexel@gmail.com>
2018-07-26 17:25:09 -0700

    Increase default gasLimit

--- Trust/Transfer/Types/GasLimitConfiguration.swift
+++ Trust/Transfer/Types/GasLimitConfiguration.swift
@@ -6,7 +6,7 @@ import BigInt
 public struct GasLimitConfiguration {
     static let `default` = BigInt(90_000)
     static let min = BigInt(21_000)
-    static let max = BigInt(300_000)
+    static let max = BigInt(600_000)
     static let tokenTransfer = BigInt(144_000)
-    static let dappTransfer = BigInt(300_000)
+    static let dappTransfer = BigInt(600_000)
 }
