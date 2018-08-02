ef83945536d981bb40bce517d96d07dc4705fe5b
Taras Pasichnyk <dev.taras.pasichnyk@gmail.com>
2018-08-02 12:02:39 +0300

    Change LockCreatePasscodeViewModel to use R.string.localizable

--- Trust/Lock/ViewModels/LockCreatePasscodeViewModel.swift
+++ Trust/Lock/ViewModels/LockCreatePasscodeViewModel.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 final class LockCreatePasscodeViewModel: LockViewModel {
-    let title = NSLocalizedString("lock.create.passcode.view.model.title", value: "Set Passcode", comment: "")
-    let initialLabelText = NSLocalizedString("lock.create.passcode.view.model.initial", value: "Enter a new passcode", comment: "")
-    let confirmLabelText = NSLocalizedString("lock.create.passcode.view.model.confirm", value: "Please re-enter your passcode", comment: "")
+    let title = R.string.localizable.lockCreatePasscodeViewModelTitle()
+    let initialLabelText = R.string.localizable.lockCreatePasscodeViewModelInitial()
+    let confirmLabelText = R.string.localizable.lockCreatePasscodeViewModelConfirm()
 }
