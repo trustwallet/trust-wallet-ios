3e67979be6fd24056ad081a3491149721a4fe1db
hewigovens <hewigovens@gmail.com>
2018-08-10 18:17:41 +0800

    Add encodeData and struct hash tests

--- Trust/EtherClient/Types/EIP712TypedData.swift
+++ Trust/EtherClient/Types/EIP712TypedData.swift
@@ -1,23 +1,25 @@
 // Copyright DApps Platform Inc. All rights reserved.
 
+//https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md
 import Foundation
+import TrustCore
 
-struct EIP712Type: Decodable {
+struct EIP712Type: Codable {
     let name: String
     let type: String
 }
 
-struct EIP712Domain: Decodable {
+struct EIP712Domain: Codable {
     let name: String
     let version: String
     let chainId: Int
     let verifyingContract: String
 }
 
-struct EIP712TypedData: Decodable {
+struct EIP712TypedData: Codable {
     let types: [String: [EIP712Type]]
     let primaryType: String
-    let domain: EIP712Domain
+    let domain: JSON
     let message: JSON
 }
 
@@ -36,13 +38,88 @@ extension EIP712TypedData {
         return found
     }
 
-    func encodeType(primaryType: String) -> String {
+    func encodeType(primaryType: String) -> Data {
         var depSet = findDependencies(primaryType: primaryType)
         depSet.remove(primaryType)
         let sorted = [primaryType] + Array(depSet).sorted()
-        return sorted.map { type in
+        let encoded = sorted.map { type in
             let param = types[type]!.map { "\($0.type) \($0.name)" }.joined(separator: ",")
             return "\(type)(\(param))"
         }.joined()
+        return encoded.data(using: .utf8) ?? Data()
+    }
+
+    func encodeData(data: JSON, type: String) -> Data {
+        let encoder = ABIEncoder()
+        var values: [ABIValue] = []
+        do {
+            let typeHash = Crypto.hash(encodeType(primaryType: type))
+            let typeHashValue = try ABIValue(typeHash, type: .bytes(32))
+            values.append(typeHashValue)
+            if let valueTypes = types[type] {
+                try valueTypes.forEach { field in
+                    if let _ = types[field.type],
+                        let json = data[field.name] {
+                        let nestEncoded = encodeData(data: json, type: field.type)
+                        values.append(try ABIValue(Crypto.hash(nestEncoded), type: .bytes(32)))
+                    } else if let value = makeABIValue(data: data[field.name], type: field.type) {
+                        values.append(value)
+                    }
+                }
+            }
+            try encoder.encode(tuple: values)
+        } catch let error {
+            print(error)
+        }
+        return encoder.data
+    }
+
+    private func makeABIValue(data: JSON?, type: String) -> ABIValue? {
+        if (type == "string" || type == "bytes"),
+            let value = data?.stringValue,
+            let valueData = value.data(using: .utf8) {
+            return try? ABIValue(Crypto.hash(valueData), type: .bytes(32))
+        } else if type == "bool",
+            let value = data?.boolValue {
+            return try? ABIValue(value, type: .bool)
+        } else if type == "address",
+            let value = data?.stringValue,
+            let address = EthereumAddress(string: value) {
+            return try? ABIValue(address, type: .address)
+        } else if type.starts(with: "uint") {
+            let size = parseIntSize(type: type, prefix: "uint")
+            if size > 0, let value = data?.floatValue {
+                return try? ABIValue(Int(value), type: .uint(bits: size))
+            }
+        } else if type.starts(with: "int") {
+            let size = parseIntSize(type: type, prefix: "int")
+            if size > 0, let value = data?.floatValue {
+                return try? ABIValue(Int(value), type: .int(bits: size))
+            }
+        } else if type.starts(with: "bytes") {
+            if let length = Int(type.dropFirst("bytes".count)),
+                let value = data?.stringValue {
+                return try? ABIValue(value.isHexEncoded ? Data(hex: value) : Data(bytes: Array(value.utf8)), type: .bytes(length))
+            }
+        }
+        //TODO array types
+        return nil
+    }
+
+    private func parseIntSize(type: String, prefix: String) -> Int {
+        guard type.starts(with: prefix) else {
+            return -1
+        }
+        guard let size = Int(type.dropFirst(prefix.count)) else {
+            if type == prefix {
+                return 256
+            }
+            return -1
+        }
+
+        if size < 8 || size > 256 || size % 8 != 0 {
+            return -1
+        }
+        return size
     }
 }
--- TrustTests/EtherClient/Types/EIP712TypedDataTests.swift
+++ TrustTests/EtherClient/Types/EIP712TypedDataTests.swift
@@ -7,6 +7,7 @@ import TrustKeystore
 
 class EIP712TypedDataTests: XCTestCase {
     var typedData: EIP712TypedData!
+    let keystore = FakeEtherKeystore()
     override func setUp() {
         super.setUp()
         let string = """
@@ -60,6 +61,43 @@ class EIP712TypedDataTests: XCTestCase {
 
     func testEncodeType() {
         let result = "Mail(Person from,Person to,string contents)Person(string name,address wallet)"
-        XCTAssertEqual(typedData.encodeType(primaryType: "Mail"), result)
+        XCTAssertEqual(typedData.encodeType(primaryType: "Mail"), result.data(using: .utf8)!)
+    }
+
+    func testEncodedTypeHash() {
+        let result = "0xa0cedeb2dc280ba39b857546d74f5549c3a1d7bdc2dd96bf881f76108e23dac2"
+        let data = typedData.encodeType(primaryType: "Mail")
+        XCTAssertEqual(Crypto.hash(data).hexEncoded, result)
+    }
+
+    func testEncodeData() {
+        let result = "0xa0cedeb2dc280ba39b857546d74f5549c3a1d7bdc2dd96bf881f76108e23dac2fc71e5fa27ff56c350aa531bc129ebdf613b772b6604664f5d8dbe21b85eb0c8cd54f074a4af31b4411ff6a60c9719dbd559c221c8ac3492d9d872b041d703d1b5aadf3154a261abdd9086fc627b61efca26ae5702701d05cd2305f7c52a2fc8"
+        let data = typedData.encodeData(data: typedData.message, type: "Mail")
+        XCTAssertEqual(data.hexEncoded, result)
+    }
+
+    func testStructHash() {
+        let result = "0xc52c0ee5d84264471806290a3f2c4cecfc5490626bf912d01f240d7a274b371e"
+        let data = typedData.encodeData(data: typedData.message, type: "Mail")
+        XCTAssertEqual(Crypto.hash(data).hexEncoded, result)
+
+        let result2 = "0xf2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090f"
+//        let json = try! JSONDecoder().decode(JSON.self, from: try! JSONEncoder().encode(typedData.domain))
+        let data2 = typedData.encodeData(data: typedData.domain, type: "EIP712Domain")
+        XCTAssertEqual(Crypto.hash(data2).hexEncoded, result2)
+    }
+
+    func testSignHash() {
+        let cow = "cow".data(using: .utf8)!
+        let privateKey = PrivateKey(data: Crypto.hash(cow))!
+        let importResult = keystore.importPrivateKey(privateKey: privateKey, password: TestKeyStore.password, coin: .ethereum)
+        guard case let .success(account1) = importResult else {
+            return XCTFail("import account failed")
+        }
+        let data = Data(bytes: [0x19, 0x01]) +
+            Crypto.hash(typedData.encodeData(data: typedData.domain, type: "EIP712Domain")) +
+            Crypto.hash(typedData.encodeData(data: typedData.message, type: "Mail"))
+        let result = "0xbe609aee343fb3c4b28e1df9e632fca64fcfaede20f02e86244efddf30957bd2"
+        XCTAssertEqual(Crypto.hash(data).hexEncoded, result)
     }
 }
